4926a9c0493b3027c62d7c793e5ed8901b457068
Matt Vollrath <matt@endpoint.com>
2015-06-17 19:51:11 -0400

    Modeline for lg_earth/setup.py

--- lg_earth/setup.py
+++ lg_earth/setup.py
@@ -6,8 +6,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['lg_earth'],
     package_dir={'': 'src'},
-    scripts=['scripts/client'],
+    scripts=['scripts/client', 'scripts/viewsync_sniffer'],
     requires=[]
 )
 
 setup(**d)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
