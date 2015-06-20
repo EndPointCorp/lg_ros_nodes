b9b78f7550d76e5a025bd9d60a4ea3dd8d32a402
Matt Vollrath <matt@endpoint.com>
2015-06-20 12:55:00 -0400

    Clean up lg_earth package/CMakeLists.txt

--- lg_earth/setup.py
+++ lg_earth/setup.py
@@ -6,8 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['lg_earth'],
     package_dir={'': 'src'},
-    scripts=['scripts/client', 'scripts/viewsync_sniffer'],
-    requires=[]
 )
 
 setup(**d)
