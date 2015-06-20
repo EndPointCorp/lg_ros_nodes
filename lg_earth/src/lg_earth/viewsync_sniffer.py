43857e805bb199d057e5a119f2e1eeb8b8a15f38
Matt Vollrath <matt@endpoint.com>
2015-06-20 17:04:47 -0400

    A more reasonable viewsync_sniffer.py

--- lg_earth/src/lg_earth/viewsync_sniffer.py
+++ lg_earth/src/lg_earth/viewsync_sniffer.py
@@ -1,67 +1,49 @@
-import SocketServer
-import threading
+from socket import *
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-
-
-def _get_publisher():
-    if not hasattr(_get_publisher, 'instance'):
-        _get_publisher.instance = rospy.Publisher(
-            '/earth/pose', PoseStamped, queue_size=3
-        )
-    return _get_publisher.instance
-
-
-def _get_repeat_target():
-    if not hasattr(_get_repeat_target, 'target'):
-        host = rospy.get_param('~repeat_host', '127.0.0.255')
-        port = rospy.get_param('~repeat_port', 42000)
-        _get_repeat_target.target = (host, port)
-    return _get_repeat_target.target
-
-
-class ViewsyncHandler(SocketServer.BaseRequestHandler):
-    def setup(self):
-        self.pub = _get_publisher()
-        self.target = _get_repeat_target()
-
-    def handle(self):
-        data = self.request[0]
-        socket = self.request[1]
-        socket.sendto(data, self.target)
-        fields = data.split(',')
-        msg = PoseStamped()
-        msg.header.stamp = rospy.get_rostime()
-        msg.pose.position.x = float(fields[1])
-        msg.pose.position.y = float(fields[2])
-        msg.pose.position.z = float(fields[3])
-        msg.pose.orientation.z = float(fields[4])
-        msg.pose.orientation.x = float(fields[5])
-        msg.pose.orientation.y = float(fields[6])
-        msg.pose.orientation.w = 0
-        self.pub.publish(msg)
-
-
-class ThreadedUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
-    pass
 
 
 class ViewsyncSniffer:
     def run(self):
         rospy.init_node('lg_earth_sniffer')
 
-        self.listen_host = rospy.get_param('~listen_host', '127.0.0.1')
-        self.port = rospy.get_param('~listen_port', 42000)
+        listen_host = rospy.get_param('~listen_host', '127.0.0.1')
+        listen_port = rospy.get_param('~listen_port', 42000)
+        repeat_host = rospy.get_param('~repeat_host', '<broadcast>')
+        repeat_port = rospy.get_param('~repeat_port', 42000)
 
-        ThreadedUDPServer.allow_reuse_address = True
-        server = ThreadedUDPServer((self.listen_host, self.port), ViewsyncHandler)
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
+        pose_pub = rospy.Publisher(
+            '/earth/pose', PoseStamped, queue_size=3
+        )
 
-        rospy.on_shutdown(server.shutdown)
+        listen_sock = socket(AF_INET, SOCK_DGRAM)
+        listen_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        listen_sock.bind((listen_host, listen_port))
 
-        rospy.spin()
+        repeat_sock = socket(AF_INET, SOCK_DGRAM)
+        repeat_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        repeat_sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+        while not rospy.is_shutdown():
+            try:
+                data = listen_sock.recv(512)
+            except error as e:
+                rospy.loginfo('socket interrupted, breaking loop')
+                break
+            repeat_sock.sendto(data, (repeat_host, repeat_port))
+
+            fields = data.split(',')
+            msg = PoseStamped()
+            msg.header.stamp = rospy.get_rostime()
+            msg.pose.position.x = float(fields[1])
+            msg.pose.position.y = float(fields[2])
+            msg.pose.position.z = float(fields[3])
+            msg.pose.orientation.z = float(fields[4])
+            msg.pose.orientation.x = float(fields[5])
+            msg.pose.orientation.y = float(fields[6])
+            msg.pose.orientation.w = 0
+            pose_pub.publish(msg)
+
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
