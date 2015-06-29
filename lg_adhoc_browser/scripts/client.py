c69769ec39161c1b60fef6a4c7c4aa8dbb45ab6c
Szymon Guz <simon@simononsoftware.com>
2015-06-29 17:40:48 +0200

    Add a basic adhoc browser node

---
+++ lg_adhoc_browser/scripts/client.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def main():
+    print ("YES")
+
+if __name__ == "__main__":
+    main()
---
+++ lg_adhoc_browser/src/lg_adhoc_browser/client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+TODO
+"""
+
+def run_node():
+    print "aaa"
+
+if __name__ == "__main__":
+    run_node()
