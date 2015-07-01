549cc0f6ac0cd1000e296b00b7e9a608ffe1e909
Matt Vollrath <matt@endpoint.com>
2015-07-01 14:20:45 -0400

    Cleanup kmlsync script

--- lg_earth/scripts/kmlsync.py
+++ lg_earth/scripts/kmlsync.py
@@ -6,7 +6,6 @@ from flask import Flask
 from lg_earth import KMLSyncServer
 from lg_common.webapp import ros_flask_spin
 
-app = Flask(__name__)
 
 def main():
     rospy.init_node('kmlsync_server', anonymous=True)
@@ -14,9 +13,8 @@ def main():
     host = rospy.get_param('~kmlsync_listen_host', '127.0.0.1')
     port = rospy.get_param('~kmlsync_listen_port', 8765)
 
+    app = Flask(__name__)
     ros_flask_spin(app, host=host, port=port, debug=True, flask_classes=[KMLSyncServer])
-
-    rospy.spin()
 
 if __name__ == '__main__':
     main()
