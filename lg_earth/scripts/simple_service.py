f3effaa0576ff70df291ca608a3b13a3dc51abe7
Jacob Minshall <jacob@endpoint.com>
2015-07-02 11:43:46 -0400

    kmlsync: smarter service
    Will return only the assets for the window_slug passed as an argument to
    the service. Returns the assets in a string array.

--- lg_earth/scripts/simple_service.py
+++ lg_earth/scripts/simple_service.py
@@ -5,7 +5,9 @@ from lg_earth import OurState
 def main():
     rospy.init_node('kml_service', anonymous=True)
 
-    s = OurState('/director/scene')
+    topic = rospy.get_param('~director_topic', '/director/scene')
+    service_channel = rospy.get_param('~service_channel', 'kmlsync/state')
+    s = OurState(topic, service_channel)
 
     rospy.spin()
 
--- lg_earth/src/lg_earth/simple_service.py
+++ lg_earth/src/lg_earth/simple_service.py
@@ -2,18 +2,35 @@
 import rospy
 from interactivespaces_msgs.msg import GenericMessage
 from lg_earth.srv import KmlState
+import json
 
 
 class OurState:
-    def __init__(self, topic):
+    def __init__(self, topic, service_channel):
+        self.topic = topic
         self.sub = rospy.Subscriber(topic, GenericMessage, self._save_state)
-        self.srv = rospy.Service('kmlsync/state', KmlState, self._process_service_request)
+        self.srv = rospy.Service(service_channel, KmlState, self._process_service_request)
         self.state = None
 
     def _save_state(self, msg):
-        self.state = KmlState()
-        self.state.type = msg.type
-        self.state.message = msg.message
+        try:
+            self.state = json.loads(msg.message)
+        except ValueError:
+            rospy.logerr("Non json value published on %s, keeping previous state" % self.topic)
 
     def _process_service_request(self, req):
-        return { 'type': self.state.type, 'message': self.state.message}
+        window_slug = req.window_slug
+        try:
+            assert isinstance(self.state, dict)
+            assert 'windows' in self.state
+        except AssertionError:
+            rospy.logerr("Invalid message has been stored, state might not have been set")
+            return {'assets': []}
+        for window in self.state['windows']:
+            if 'presentation_viewport' not in window or 'assets' not in window:
+                continue
+            if not window['presentation_viewport'] == window_slug:
+                continue
+            return {'assets': window['assets']}
+        # couldn't find specific window_slug inside state
+        return {'assets': []}
