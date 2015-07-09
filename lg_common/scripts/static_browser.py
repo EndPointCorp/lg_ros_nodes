fa2fdda7f6b374757c1d60e86a5420146a86853b
Neil Elliott <neil@endpoint.com>
2015-07-09 15:54:36 -0400

    Typo correction

--- lg_common/scripts/static_browser.py
+++ lg_common/scripts/static_browser.py
@@ -9,25 +9,17 @@ from std_msgs.msg import String
 if __name__ == '__main__':
     rospy.init_node('static_browser')
 
-    url = rospy.getparam('~url', None)
-    window_x = rospy.getparam('~window_x', None)
-    window_y = rospy.getparam('~window_y', None)
-    window_w = rospy.getparam('~window_w', None)
-    window_h = rospy.getparam('~window_h', None)
+    url = rospy.get_param('~url', None)
+    viewport = rospy.get_param('~viewport', None)
 
-    geometry = WindowGeometry(
-        x=window_x,
-        y=window_x,
-        width=window_w,
-        height=window_height
-    )
-
-    user_agent = rospy.getparam(
+    user_agent = rospy.get_param(
         '~user_agent', 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; '
-        'en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) ' +
+        'en-us AppleWebKit/531.21.10 (KHTML, like Gecko) ' +
         'Version/4.0.4 Mobile/7B314 Safari/531.21.10'
     )
-    debug_port = rospy.getparam('~debug_port', 10000),
+    debug_port = rospy.get_param('~debug_port', 10000),
+
+    state = rospy.get_param('~state', ApplicationState.VISIBLE)
 
     browser = ManagedBrowser(
         url=url,
@@ -36,9 +28,9 @@ if __name__ == '__main__':
         debug_port=debug_port,
     )
 
-    browser.set_state(ApplicateionState.VISIBLE)
+    browser.set_state(state)
 
-    rospy.Subscriber('{}/state'.format(rospy.getname()), ApplicationState,
+    rospy.Subscriber('{}/state'.format(rospy.get_name()), ApplicationState,
                      browser.handle_state_msg)
 
     def handle_debug_sock_msg(msg):
