e5dfb2fed3d5a55d75ed013381c4cab76d0698f2
Neil Elliott <neil@endpoint.com>
2015-07-10 10:24:02 -0400

    Added geometry, updated dev.launch with TS

--- lg_common/scripts/static_browser.py
+++ lg_common/scripts/static_browser.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
 
 import rospy
-from lg_common import ManagedBrowser
-from lg_common.msg import ApplicationState
+
+from lg_common import ManagedBrowser, ManagedWindow
+from lg_common.msg import ApplicationState, WindowGeometry
 from std_msgs.msg import String
 
 
 if __name__ == '__main__':
     rospy.init_node('static_browser')
 
+    geometry = ManagedWindow.get_viewport_geometry()
     url = rospy.get_param('~url', None)
-    viewport = rospy.get_param('~viewport', None)
-
+    scale_factor = rospy.get_param('~force_device_scale_factor', 2)
+    debug_port = rospy.get_param('~debug_port', 10000)
     user_agent = rospy.get_param(
         '~user_agent', 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; '
         'en-us AppleWebKit/531.21.10 (KHTML, like Gecko) ' +
         'Version/4.0.4 Mobile/7B314 Safari/531.21.10'
     )
-    debug_port = rospy.get_param('~debug_port', 10000),
-
     state = rospy.get_param('~state', ApplicationState.VISIBLE)
 
     browser = ManagedBrowser(
+        geometry=geometry,
         url=url,
-        force_device_scale_factor=2,
-        user_agent=user_agent,
+        force_device_scale_factor=scale_factor,
         debug_port=debug_port,
+        user_agent=user_agent,
     )
 
     browser.set_state(state)
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     def handle_debug_sock_msg(msg):
         browser.send_debug_sock_msg(msg.data)
 
-    rospy.Subscriber('/example_browser/debug', String, handle_debug_sock_msg)
+    rospy.Subscriber('{}/debug'.format(rospy.get_name()), String, handle_debug_sock_msg)
 
     rospy.spin()
 
