d1db6f2bdae80fd44c4d6bf7feeb005e85790b87
Kannan Ponnusamy <kannan4k@gmail.com>
2015-07-13 07:45:53 -0400

    Added streetview launcher script

---
+++ lg_sv/scripts/launcher.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import rospy
+
+from lg_common import ManagedWindow, ManagedBrowser
+from lg_common.msg import ApplicationState
+
+def main():
+    rospy.init_node('streetview_browser', anonymous=True)
+    geometry = ManagedWindow.get_viewport_geometry()
+    url = rospy.get_param('~url','http://maps.google.com')
+    yawOffset = rospy.get_param('~yaw_offset', 0)
+    pitchOffset = rospy.get_param('~pitch_offset', 0)
+    showLinks = rospy.get_param('~show_links', False)
+    # put parameters into one big url
+    url = "%s?yawOffset=%s&pitchOffset=%s&showLinks=%s" % (url, yawOffset, pitchOffset, str(showLinks).lower())
+    # create the managed browser
+    managed_browser = ManagedBrowser(url=url, geometry=geometry)
+
+    # set to visible
+    state = ApplicationState.VISIBLE
+    managed_browser.set_state(state)
+
+    # listen to state messages
+    rospy.Subscriber('/streetview/state', ApplicationState, managed_browser.handle_state_msg)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
