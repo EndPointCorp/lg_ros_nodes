6a85208da483774bc4f601488145fa3aaa567fa7
Joshua Tolley <josh@endpoint.com>
2015-07-13 15:39:32 -0600

    Skeleton of a pano server

--- lg_pano/scripts/master.py
+++ lg_pano/scripts/master.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import String
 from lg_pano import PanoMaster
 
 if __name__ == '__main__':
     rospy.init_node('pano_master')
-    pm = PanoMaster()
+
+    # Does it really matter what the queue size is?
+    pov  = rospy.Publisher('/panoviewer/pov',  Vector3, queue_size=10)
+    pano = rospy.Publisher('/panoviewer/pano', String,  queue_size=10)
+
+    pm = PanoMaster(pov, pano)
+    # XXX The issue says we need to listen to this, but what's the msg type?
+    # rospy.Subscriber(
+    #     '/panoviewer/state',
+    # )
+    rospy.Subscriber(
+        '/panoviewer/init',
+        String,
+        pm.initApp
+    )
     rospy.Subscriber(
         '/spacenav/twist',
         Twist,
--- lg_pano/src/lg_pano/master.py
+++ lg_pano/src/lg_pano/master.py
@@ -1,9 +1,35 @@
 import rospy
 
+from geometry_msgs.msg import Vector3
+from std_msgs.msg import String
+
 class PanoMaster:
+    def __init__(self, pov, pano):
+        self.povPublisher  = pov
+        self.panoPublisher = pano
+        self.pov = Vector3(0, 0, 0)
+        self.panoUrl = String("")
+
     def spacenavTwist(self, msg):
         if (msg.angular.x == 0 and msg.angular.y == 0 and msg.angular.z == 0):
             return 
-        rospy.loginfo('Received twist msg: %s', msg)
+        self.pov.x += msg.angular.x
+        self.pov.y += msg.angular.y
+        self.pov.z += msg.angular.z
+        self.sendPov()
+
+    def sendPano(self):
+        self.panoPublisher.publish(self.panoUrl)
+
+    def sendPov(self):
+        self.povPublisher.publish(self.pov)
+
+    def initApp(self, msg):
+        # A new instance has connected; send it the current pano and pov
+        # It doesn't really matter what the new instance sends; the presence of
+        # any message tells us to send these messages.
+        rospy.loginfo('Received init msg: %s', msg.data)
+        self.sendPano()
+        self.sendPov()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
