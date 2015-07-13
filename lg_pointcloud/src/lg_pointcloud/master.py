4a9cb9527deb8a34bc5441608a14945ea6d93ea9
Joshua Tolley <josh@endpoint.com>
2015-07-13 10:44:10 -0600

    Minor fiddling

--- lg_pointcloud/src/lg_pointcloud/master.py
+++ lg_pointcloud/src/lg_pointcloud/master.py
@@ -2,4 +2,6 @@ import rospy
 
 class PointcloudMaster:
     def spacenavTwist(self, msg):
+        if (msg.angular.x == 0 and msg.angular.y == 0 and msg.angular.z == 0):
+            return 
         rospy.loginfo('Received twist msg: %s', msg)
