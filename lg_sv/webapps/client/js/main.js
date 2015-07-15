537faa93091688d33e9751734516ab7aba61a450
Matt Vollrath <matt@endpoint.com>
2015-07-15 17:59:12 -0400

    Add Street View webapp

---
+++ lg_sv/webapps/client/js/main.js
@@ -0,0 +1,68 @@
+function getParameterByName(name, type, def) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null ? def : type(decodeURIComponent(results[1].replace(/\+/g, " ")));
+}
+
+function stringToBoolean(s) {
+  var truePattern = /^1$|^true&/i;
+  return s.search(truePattern) === 0;
+}
+
+var showLinks = getParameterByName('showLinks', stringToBoolean, false);
+var yawOffset = getParameterByName('yawOffset', Number, 0);
+var pitchOffset = getParameterByName('pitchOffset', Number, 0);
+
+function initialize() {
+  console.log('initializing Street View');
+
+  var canvas = document.getElementById('map-canvas');
+
+  var mapOptions = {
+    disableDefaultUI: true,
+    center: new google.maps.LatLng(45,45),
+    backgroundColor: "black",
+    zoom: 8
+  };
+  var map = new google.maps.Map(canvas, mapOptions);
+
+  var svOptions = {
+    visible: true,
+    disableDefaultUI: true,
+    linksControl: showLinks
+  };
+  var sv = new google.maps.StreetViewPanorama(canvas, svOptions);
+
+  map.setStreetView(sv);
+
+  var ros = new ROSLIB.Ros({
+    url: 'ws://localhost:9090'
+  });
+  ros.on('connection', function() {
+    console.log('ROSLIB connected');
+  });
+
+  var svClient = new StreetviewClient(ros, sv);
+
+  svClient.on('pano_changed', function(panoId) {
+    console.log('Changing pano to', panoId);
+    sv.setPano(panoId);
+  });
+
+  svClient.on('pov_changed', function(povQuaternion) {
+    var pov = {
+      heading: povQuaternion.y,
+      pitch: povQuaternion.x
+    };
+    var roll = povQuaternion.z;
+    var zoom = povQuaternion.w;
+    console.log('Changing pov to', pov, roll, zoom);
+    sv.setPov(pov);
+    sv.setZoom(zoom);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+// # vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
---
+++ lg_sv/webapps/lib/sv_client.js
@@ -0,0 +1,100 @@
+/**
+ * Street View client library.
+ *
+ * Depends upon EventEmitter2 and ROSLIB.
+ *
+ * @constructor
+ * @param {ROSLIB.Ros} ros
+ * @param {google.maps.StreetViewPanorama} streetView
+ */
+function StreetviewClient(ros, streetView) {
+  this.streetView = streetView;
+
+  this.povTopic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/streetview/pov',
+    messageType: 'geometry_msgs/Quaternion',
+    throttle_rate: 16,
+    queue_length: 1
+  });
+  this.panoTopic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/streetview/panoid',
+    messageType: 'std_msgs/String',
+    throttle_rate: 16,
+    queue_length: 1
+  });
+  this.metadataTopic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/streetview/metadata',
+    messageType: 'std_msgs/String',
+    throttle_rate: 16,
+    queue_length: 1
+  });
+
+  // Linkage
+  this.povTopic.subscribe(this.handlePovMsg.bind(this));
+  this.panoTopic.subscribe(this.handlePanoMsg.bind(this));
+  this.metadataTopic.advertise();
+  this.streetView.addListener('links_changed', this.sendMetadata.bind(this));
+}
+
+StreetviewClient.prototype.__proto__ = EventEmitter2.prototype;
+
+/**
+ * Handles a pov message from the ROS graph.
+ * @param {object} msg
+ *       geometry_msgs/Quaternion representation of pov
+ */
+StreetviewClient.prototype.handlePovMsg = function(msg) {
+  this.emit('pov_changed', msg);
+};
+
+/**
+ * Handles a pano message from the ROS graph.
+ * @param {object} msg
+ *       std_msgs/String representation of panoId
+ */
+StreetviewClient.prototype.handlePanoMsg = function(msg) {
+  var panoId = msg.data;
+  this.emit('pano_changed', panoId);
+};
+
+/**
+ * Publishes metadata for the current pano to the ROS graph.
+ */
+StreetviewClient.prototype.sendMetadata = function() {
+  var links = this.streetView.getLinks();
+  var panoLocation = this.streetView.getLocation();
+  var photographerPov = this.streetView.getPhotographerPov();
+  var metadata = {
+    links: [],
+    location: {
+      latLng: {
+        lat: panoLocation.latLng.lat(),
+        lng: panoLocation.latLng.lng()
+      },
+      pano: panoLocation.pano,
+      description: panoLocation.description,
+      shortDescription: panoLocation.shortDescription
+    },
+    photographerPov: {
+      heading: photographerPov.heading,
+      pitch: photographerPov.pitch
+    }
+  };
+  for(var i = 0; i < links.length; i++) {
+    var link = links[i];
+    metadata.links.push({
+      description: link.description,
+      heading: link.heading,
+      pano: link.pano
+    });
+  }
+  var msg = new ROSLIB.Message({
+    data: JSON.stringify(metadata)
+  });
+  this.metadataTopic.publish(msg);
+};
+
+// # vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
