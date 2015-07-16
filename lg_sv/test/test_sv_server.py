5a96f755ffa08a6c0e3fc59a805d6a2a1546163a
Jacob Minshall <minshalljg@gmail.com>
2015-07-16 06:48:52 -0700

    test for panoid publisher

--- lg_sv/test/test_sv_server.py
+++ lg_sv/test/test_sv_server.py
@@ -5,15 +5,32 @@ NAME = 'test_sv_server'
 LAT = '43.726319'
 LON = '12.636377'
 
+TILT_MAX = 80
+TILT_MIN = -80
+NAV_SENSITIVITY = 1
+NAV_INTERVAL = .1
+
 import sys
 import rospy
 import rostest
 import unittest
-from lg_sv import StreetviewUtils
+from lg_sv import StreetviewUtils, StreetviewServer
+
+class MockPublisher:
+    def __init__(self):
+        self.data = []
+
+    def publish(self, data):
+        self.data.append(data)
 
 class TestSVServer(unittest.TestCase):
     def setUp(self):
-        pass
+        self.location_pub = MockPublisher()
+        self.pano_pub = MockPublisher()
+        self.pov_pub = MockPublisher()
+        self.server = StreetviewServer(
+             self.location_pub, self.pano_pub, self.pov_pub, TILT_MAX,
+             TILT_MIN, NAV_SENSITIVITY, NAV_INTERVAL)
     
     def tearDown(self):
         pass
@@ -21,6 +38,22 @@ class TestSVServer(unittest.TestCase):
     def test_sv_utils(self):
         response = StreetviewUtils.get_panoid_from_lat_lon(LAT, LON)
         self.assertIsInstance(response, str)
+
+    def test_1_pano_pub(self):
+        # test initial state
+        self.assertEqual(len(self.pano_pub.data), 0,
+                         'pano_pub data should not have anything in it')
+        self.assertEqual(self.server.panoid, str(),
+                         'panoid should not be set yet')
+        new_pano = 'foo_bar'
+        self.server.pub_panoid(new_pano)
+        # test final state
+        self.assertEqual(len(self.pano_pub.data), 1,
+                         'pano_pub data did not have the correct amount of data')
+        self.assertEqual(self.server.panoid, new_pano,
+                         'panoid should not be set yet')
+        self.assertEqual(self.pano_pub.data[0], new_pano,
+                         'pano_pub data did not have the correct value')
 
 
 if __name__ == '__main__':
