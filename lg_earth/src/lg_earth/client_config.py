52a95c1045cc28f47aaa93440ef260886ced3f4f
Matt Vollrath <matt@endpoint.com>
2015-07-21 12:31:49 -0400

    Docstrings and nomenclature in lg_earth

--- lg_earth/src/lg_earth/client_config.py
+++ lg_earth/src/lg_earth/client_config.py
@@ -7,11 +7,20 @@ import rospy
 
 
 class ClientConfig:
+    """
+    Configuration helper for Google Earth client.
+    """
     def __init__(self, base_path, instance_name):
         self.base_path = base_path
         self.instance_name = instance_name
 
     def get_config(self):
+        """
+        Generate configuration based on ROS parameters.
+
+        Returns:
+            Tuple of configuration maps.
+        """
         args = []
 
         args.extend([
