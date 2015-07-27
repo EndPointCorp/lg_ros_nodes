918e5f0cff947c4c8718f6ce98d579d0c3eedfae
Jacob Minshall <minshalljg@gmail.com>
2015-07-27 09:15:34 -0700

    kmlsync: send playtourqueryrequest object instead of string

--- lg_earth/src/lg_earth/kmlsync_state.py
+++ lg_earth/src/lg_earth/kmlsync_state.py
@@ -2,7 +2,7 @@
 import rospy
 from interactivespaces_msgs.msg import GenericMessage
 from std_msgs.msg import String
-from lg_earth.srv import KmlState, PlaytourQuery
+from lg_earth.srv import KmlState, PlaytourQueryRequest
 import json
 
 
@@ -18,7 +18,7 @@ class KmlSyncState:
             assert 'windows' in state
             self.state = state
             # send blank playtour query to unload any kmls
-            self._send_playtour_query(str())
+            self._send_playtour_query(PlaytourQueryRequest())
         except AssertionError:
             rospy.logerr('Invalid message - keeping previous state')
         except ValueError:
