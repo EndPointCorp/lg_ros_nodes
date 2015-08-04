765216e43f545069b94c0f4c0f3135bbc29b4997
Wojciech Ziniewicz <wojtek@endpoint.com>
2015-08-04 08:12:47 -0400

    AdhocBrowser
    - added more docstrings
    - added files for rests
    - introduced nosy.cfg for clever nosetesting

--- lg_common/src/lg_common/managed_adhoc_browser.py
+++ lg_common/src/lg_common/managed_adhoc_browser.py
@@ -14,7 +14,7 @@ class ManagedAdhocBrowser(ManagedBrowser):
     def __init__(self, geometry, slug, url):
         self.slug = slug
         self.url = url
-        self.geometry = self._add_offset_to_geometry(geometry)
+        self.geometry = geometry
 
         super(ManagedAdhocBrowser, self).__init__(
                 geometry=geometry,
@@ -22,23 +22,12 @@ class ManagedAdhocBrowser(ManagedBrowser):
                 url=url,
                 kiosk=True)
 
-    def _add_offset_to_geometry(self, supplied_geometry):
-        """
-        Adhoc browser needs geometry that's honoring viewport offsets
-        This method will add offsets to original geometry
-        """
-        viewport_geometry = ManagedWindow.get_viewport_geometry()
-        supplied_geometry.x += viewport_geometry.x
-        supplied_geometry.y += viewport_geometry.y
-
-        return supplied_geometry
-
     def update_geometry(self, geometry):
         """
         ManagedAdhocBrowser << ManagedBrowser << ManagedApplication.window.geometry = 'WxH[+-]X[+-]Y'
         ManagedAdhocBrowser << ManagedBrowser << ManagedApplication.window.converge()
         """
-        self.window.geometry = self._add_offset_to_geometry(geometry)
+        self.window.geometry = geometry
         self.window.converge(geometry)
 
     def update_url(self, url):
