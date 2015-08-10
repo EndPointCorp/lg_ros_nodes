33e7f773c1ce489367349720b4b2fd742676cadf
Will Plaut <will@endpoint.com>
2015-08-10 16:41:02 -0400

    Remove faulty KML unload

--- lg_earth/src/lg_earth/kmlsync_state.py
+++ lg_earth/src/lg_earth/kmlsync_state.py
@@ -17,8 +17,6 @@ class KmlSyncState:
             assert isinstance(state, dict)
             assert 'windows' in state
             self.state = state
-            # send blank playtour query to unload any kmls
-            self._send_playtour_query(PlaytourQueryRequest())
         except AssertionError:
             rospy.logerr('Invalid message - keeping previous state')
         except ValueError:
