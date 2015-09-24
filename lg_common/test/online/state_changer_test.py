e31bb422020d3b48ba124b23f074ac119717b741
Jacob Minshall <minshalljg@gmail.com>
2015-09-24 10:45:00 -0700

    state_changer: test: sleep before publishing
    The array based mock state uncovered an issue, the very first time these
    publishers were created, they didn't actually publish anything, even
    with the wait_for_pub in there.

--- lg_common/test/online/state_changer_test.py
+++ lg_common/test/online/state_changer_test.py
@@ -42,11 +42,16 @@ class TestStateChanger(unittest.TestCase):
         self.set_all_state_as(ApplicationState.HIDDEN)
 
     def set_all_state_as(self, state):
+        ps = []
         for sub in self.subs:
-            self.wait_for_pubsub(sub[SUB].name)
+            self.wait_for_sub(sub[SUB].name)
             p = rospy.Publisher(sub[SUB].name, ApplicationState, queue_size=2)
-            p.publish(state)
+            self.wait_for_pub(p.name)
+            ps.append(p)
         # give the subscribers time to finish
+        rospy.sleep(1)
+        for p in ps:
+            p.publish(state)
         rospy.sleep(1)
 
     def tearDown(self):
@@ -107,9 +112,10 @@ class TestStateChanger(unittest.TestCase):
     def test_active_not_found(self):
         active = ['/foooooo/bar']
         self.publish_active(active)
+        self.check_state_length(1)
         self.test_all_start_as_hidden()
 
-    def wait_for_pubsub(self, pubtopic):
+    def wait_for_sub(self, pubtopic):
         # wait at most 5 seconds for listenerpublisher to be registered
         timeout_t = time.time() + 5.0
         while not rostest.is_subscriber(
@@ -118,6 +124,18 @@ class TestStateChanger(unittest.TestCase):
             time.sleep(0.1)
 
         self.assert_(rostest.is_subscriber(
+            rospy.resolve_name(pubtopic),
+            rospy.resolve_name(LPNODE)), "%s is not up" % LPNODE)
+
+    def wait_for_pub(self, pubtopic):
+        # wait at most 5 seconds for listenerpublisher to be registered
+        timeout_t = time.time() + 5.0
+        while not rostest.is_publisher(
+                rospy.resolve_name(pubtopic),
+                rospy.resolve_name(LPNODE)) and time.time() < timeout_t:
+            time.sleep(0.1)
+
+        self.assert_(rostest.is_publisher(
             rospy.resolve_name(pubtopic),
             rospy.resolve_name(LPNODE)), "%s is not up" % LPNODE)
 
