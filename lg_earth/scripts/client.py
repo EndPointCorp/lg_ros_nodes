51bbf5267738807e76d1948d92fe8f7ef9149ea3
Wojciech Ziniewicz <wojciech.ziniewicz@gmail.com>
2015-09-25 01:55:15 -0400

    Added ext dependency mechanism and added it to GE and SV/PV

--- lg_earth/scripts/client.py
+++ lg_earth/scripts/client.py
@@ -3,10 +3,22 @@
 import rospy
 from lg_earth import Client
 from lg_common.msg import ApplicationState
+from lg_common.helpers import DependencyException
 
 
 def main():
     rospy.init_node('lg_earth')
+
+    kmlsync_host = 'localhost'
+    kmlsync_port = rospy.get_param('/kmlsync_server/port', 8765)
+    kmlsync_timeout = rospy.get_param('/global_dependency_timeout', 15)
+
+    if not depend_on_service(kmlsync_host, kmlsync_port, 'kmlsync', kmlsync_timeout):
+        msg = "Service: %s didnt get accessible within %s seconds" % ('kmlsync', kmlsync_timeout)
+        rospy.lofatal(msg)
+        raise DependencyException(msg)
+    else:
+        rospy.loginfo("KMLsync server is active - initializing Earth")
 
     client = Client()
 
