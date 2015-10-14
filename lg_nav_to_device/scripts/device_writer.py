7be509e1331085ad06c53fdf096d249ec5e64012
Jacob Minshall <jacob@endpoint.com>
2015-10-14 15:17:33 -0400

    lg_nav: disables spacenav input when not visible

--- lg_nav_to_device/scripts/device_writer.py
+++ lg_nav_to_device/scripts/device_writer.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 from lg_nav_to_device import DeviceWriter
+from lg_common.msg import ApplicationState
 
 
 def main():
@@ -12,6 +13,7 @@ def main():
 
     device_writer = DeviceWriter(scale)
     sub = rospy.Subscriber('/spacenav/twist', Twist, device_writer.make_event)
+    rospy.Subscriber('/earth/state', ApplicationState, device_writer.set_state)
 
     rospy.spin()
 
