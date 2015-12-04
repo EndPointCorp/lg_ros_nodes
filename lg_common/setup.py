a06121ae3414183158e5f01b97fd42cc7accbe19
Jacob Minshall <minshalljg@gmail.com>
2015-12-04 11:20:30 -0800

    command_handler: added code to command and listener node

--- lg_common/setup.py
+++ lg_common/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['lg_common'],
     package_dir={'': 'src'},
-    scripts=[],
+    scripts=['bin/lg-code-to-command'],
     requires=[]
 )
 
