b070eb8b832234ca2f6300448a36ca3fd5340117
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-02-24 01:19:51 +0500

    Docker image with lg_ros_nodes

---
+++ docker/image_scripts/shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sess=docker
+wn=roslaunch
+
+tmux list-session 2>&1 | grep -q "^$sess" || tmux new-session -s $sess -d
+tmux list-window -t $sess 2>&1 | grep -q ": $wn \[" || tmux new-window -t $sess -n $wn
+tmux send-keys -t $sess:$wn "roslaunch --screen lg_common/launch/dev.launch broadcast_addr:=127.0.0.255" Enter
+tmux attach-session -t $sess
