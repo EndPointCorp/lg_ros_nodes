b4f1541ea244edb2d41f288d4507dbc72f2de603
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-07-15 17:34:35 +0500

    Add unhide browswers on ready callback

--- lg_common/src/lg_common/adhoc_browser_pool.py
+++ lg_common/src/lg_common/adhoc_browser_pool.py
@@ -14,6 +14,12 @@ class AdhocBrowserPool():
     """
     Handles browser pool in self.browsers
     Dict(id => ManagedAdhocBrowser)
+
+    Smooth transitions overview:
+    - handle_ros_message creates new browsers
+    - wait for ready signal (see rediness.py)
+    - when new browsers ready, hide and destroy
+      old browsers and show new
     """
     def __init__(self, viewport_name):
         """
@@ -87,21 +93,56 @@ class AdhocBrowserPool():
         rospy.loginfo("POOL %s: state after addition of %s: %s" % (self.viewport_name, new_browser_pool_id, self.browsers))
         return True
 
+    def _hide_browsers(self, ids):
+        """
+        Hide old browsers
+        """
+        for browser_pool_id in ids:
+            rospy.loginfo("Hiding browser with id %s" % browser_pool_id)
+            self.browsers[browser_pool_id].set_state(ApplicationState.HIDDEN)
+
+    def _destroy_browsers(self, ids):
+        """
+        Destroy browsers instances,
+        free system resources
+        """
+        rospy.loginfo("POOL %s: browsers to remove = %s" % (self.viewport_name, ids))
+        for browser_pool_id in ids:
+            rospy.loginfo("Removing browser id %s" % browser_pool_id)
+            self._remove_browser(browser_pool_id)
+
+
     def unhide_browsers(self, data):
         """
         Listen on a topic that carries following Ready type:
-         -----
-         scene_slug: 659f6d8d-7c40-4f2c-911b-49390ac13e5f__tvn24
-         activity_type: browser
-         instances: ['50220834']
-         -----
+        -----
+          scene_slug: 659f6d8d-7c40-4f2c-911b-49390ac13e5f__tvn24
+          activity_type: browser
+          instances: ['50220834']
+        -----
 
-         Activate browser instances mentioned in 'instances' that belong to 'scene_slug'
+        Activate browser instances mentioned in 'instances' that belong to 'scene_slug'
+        and remove old browsers
         """
         if data.scene_slug == self.last_scene_slug:
+
             for browser_pool_id in data.instances:
                 rospy.loginfo("Unhiding browser with id %s" % instance_name)
                 self.browsers[browser_pool_id].set_state(ApplicationState.VISIBLE)
+
+            # For now there is no scene_slug => browsers[] tracking
+            # so all the browsers who not mentioned in instances
+            # treated as old and should be hided.
+
+            old_browsers = set(self.browsers.keys()) - set(data.instances)
+            self._hide_browsers(old_browsers)
+            self._destroy_browsers(old_browsers)
+        else:
+            # That's possible if we've got new scene, before
+            # the old scene was activated
+            # (before the browsers for old scene become ready).
+            pass
+
 
     def handle_ros_message(self, data):
         """
@@ -119,6 +160,7 @@ class AdhocBrowserPool():
 
         """
 
+
         incoming_browsers = self._unpack_incoming_browsers(data.browsers)
         incoming_browsers_ids = set(incoming_browsers.keys())  # set
         current_browsers_ids = get_app_instances_ids(self.browsers)  # set
@@ -129,16 +171,8 @@ class AdhocBrowserPool():
             rospy.loginfo("Creating browser with id %s" % browser_pool_id)
             self._create_browser(browser_pool_id, incoming_browsers[browser_pool_id])
 
-        # hide old browsers
-        for browser_pool_id in current_browsers_ids:
-            rospy.loginfo("Hiding browser with id %s" % browser_pool_id)
-            self.browsers[browser_pool_id].set_state(ApplicationState.HIDDEN)
-
-        # destroy old browsers
-        rospy.loginfo("POOL %s: browsers to remove = %s" % (self.viewport_name, current_browsers_ids))
-        for browser_pool_id in current_browsers_ids:
-            rospy.loginfo("Removing browser id %s" % browser_pool_id)
-            self._remove_browser(browser_pool_id)
+        # wait for readiness signal before hide old browsers
+        self.last_scene_slug = data.scene_slug
 
         return True
 
