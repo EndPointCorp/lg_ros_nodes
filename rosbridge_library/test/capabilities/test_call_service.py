9244ff068416ba102329c867e5e0849936067708
Wojciech Ziniewicz <wojtek@endpoint.com>
2016-08-31 13:27:07 +0000

    Revert "Removed rosbridge"
    This reverts commit bf6f8e22b7b8bc182efc7339a89979fc116552bb.

---
+++ rosbridge_library/test/capabilities/test_call_service.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import sys
+import rospy
+import rostest
+import unittest
+import time
+
+from roscpp.srv import GetLoggers
+
+from json import loads, dumps
+from std_msgs.msg import String
+
+from rosbridge_library.capabilities.call_service import CallService
+from rosbridge_library.protocol import Protocol
+from rosbridge_library.protocol import InvalidArgumentException, MissingArgumentException
+
+
+class TestCallService(unittest.TestCase):
+
+    def setUp(self):
+        rospy.init_node("test_call_service")
+
+    def test_missing_arguments(self):
+        proto = Protocol("test_missing_arguments")
+        s = CallService(proto)
+        msg = loads(dumps({"op": "call_service"}))
+        self.assertRaises(MissingArgumentException, s.call_service, msg)
+
+    def test_invalid_arguments(self):
+        proto = Protocol("test_invalid_arguments")
+        s = CallService(proto)
+
+        msg = loads(dumps({"op": "call_service", "service": 3}))
+        self.assertRaises(InvalidArgumentException, s.call_service, msg)
+
+    def test_call_service_works(self):
+        # First, call the service the 'proper' way
+        p = rospy.ServiceProxy("/rosout/get_loggers", GetLoggers)
+        ret = p()
+
+
+        proto = Protocol("test_call_service_works")
+        s = CallService(proto)
+        msg = loads(dumps({"op": "call_service", "service": "/rosout/get_loggers"}))
+
+        received = {"msg": None}
+
+        def cb(msg, cid=None):
+            received["msg"] = msg
+
+        proto.send = cb
+
+        s.call_service(msg)
+
+        time.sleep(0.5)
+
+        self.assertTrue(received["msg"]["result"])
+        for x, y in zip(ret.loggers, received["msg"]["values"]["loggers"]):
+            self.assertEqual(x.name, y["name"])
+            self.assertEqual(x.level, y["level"])
+
+    def test_call_service_fail(self):
+        proto = Protocol("test_call_service_fail")
+        s = CallService(proto)
+        send_msg = loads(dumps({"op": "call_service", "service": "/rosout/set_logger_level", "args": '["ros", "invalid"]'}))
+
+        received = {"msg": None}
+
+        def cb(msg, cid=None): 
+            received["msg"] = msg
+
+        proto.send = cb
+
+        s.call_service(send_msg)
+
+        time.sleep(0.5)
+
+        self.assertFalse(received["msg"]["result"])
+
+
+PKG = 'rosbridge_library'
+NAME = 'test_call_service'
+if __name__ == '__main__':
+    rostest.unitrun(PKG, NAME, TestCallService)
+
