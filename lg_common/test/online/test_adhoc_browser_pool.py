eef18e06f50259e75ad786de008021d30cb23c40
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-09-15 13:22:49 -0300

    Fix tests

--- lg_common/test/online/test_adhoc_browser_pool.py
+++ lg_common/test/online/test_adhoc_browser_pool.py
@@ -26,12 +26,13 @@ class TestAdhocBrowserPool(unittest.TestCase):
         self.pool._create_browser('test_test', test_browser_msg)
         assert self.pool.browsers['test_test'] is not None
 
-        print "Test browser url is: %s" % self.pool.browsers['test_test'].url
-
-        assert 'test_test' in self.pool.browsers['test_test'].url
-        assert 'rosbridge_secure=1' in self.pool.browsers['test_test'].url
-        assert 'rosbridge_port=1234' in self.pool.browsers['test_test'].url
-
+        try:
+            assert 'test_test' in self.pool.browsers['test_test'].url
+            assert 'rosbridge_secure=1' in self.pool.browsers['test_test'].url
+            assert 'rosbridge_port=1234' in self.pool.browsers['test_test'].url
+        except AssertionError as e:
+            e.message = e.message + " The url is: %s" % self.pool.browsers['test_test'].url
+            raise e
 
 if __name__ == '__main__':
     rospy.init_node(NAME)
