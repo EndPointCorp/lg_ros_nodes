1cbcfba72b3b4d8fb954b427abc282207884c3b5
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-09-21 09:42:53 -0300

    rename

--- lg_common/src/lg_common/extensions/tactile_smooth_loader/tactile-ready.js
+++
@@ -1,6 +0,0 @@
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.action == 'send_ready_msg') {
-    window.postMessage({ type: "DIRECTOR_WINDOW_READY" }, "*");
-    sendResponse({"ack": true});
-  }
-});
