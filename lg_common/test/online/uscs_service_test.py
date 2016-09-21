d056fa3dd0778078a92ca34dd4dc0fcdfd274603
Wojciech Ziniewicz <wojtek@endpoint.com>
2016-09-21 19:48:44 +0200

    Amended tests to honor new behavior of uscs service

--- lg_common/test/online/uscs_service_test.py
+++ lg_common/test/online/uscs_service_test.py
@@ -105,12 +105,14 @@ class TestUSCSService(unittest.TestCase):
 
     def test_4_active_state(self):
         """
+        Sending `active=True` should not send a message because
+        uscs service is active by default after initialization
         """
         self.activity_publisher.publish(Bool(data=True))
         rospy.sleep(self.msg_emission_grace_time)
         self.assertEqual(len(self.connectivity_mock_subscriber.state), 0)
-        self.assertEqual(len(self.activity_mock_subscriber.state), 1)
-        self.assertEqual(len(self.director_mock_subscriber.state), 1)
+        self.assertEqual(len(self.activity_mock_subscriber.state), 0)
+        self.assertEqual(len(self.director_mock_subscriber.state), 0)
 
     def test_5_inactive_state(self):
         """
