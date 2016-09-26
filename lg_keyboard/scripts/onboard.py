d5508049c9be0d078945c807551e663332989e07
Wojciech Ziniewicz <wojtek@endpoint.com>
2016-09-26 13:35:24 +0200

    Onboard launcher #127
    - added OnboardLauncher executor
    - made methods for showing and hiding launcher

--- lg_keyboard/scripts/onboard.py
+++ lg_keyboard/scripts/onboard.py
@@ -6,11 +6,29 @@ from lg_common.msg import StringArray
 
 from lg_keyboard import ROS_NODE_NAME
 from lg_keyboard import OnboardManager
+from lg_keyboard import OnboardLauncher
+from lg_keyboard import OnboardViewportException
 
 
 def main():
     rospy.init_node(ROS_NODE_NAME)
-    onboard_mgr = OnboardManager()
+    viewport = rospy.get_param("~viewport", None)
+    config = rospy.get_param("~config", None)
+
+    if not viewport:
+        message = "No viewport set for OnboardManager - dying"
+        rospy.logerror(message)
+        raise OnboardViewportException(message)
+
+    onboard_launcher = OnboardLauncher(
+        viewport=viewport,
+        config=config
+    )
+    onboard_mgr = OnboardManager(
+        viewport=viewport,
+        launcher=onboard_launcher
+    )
+
     onboard_activate_subscriber = rospy.Subscriber("/lg_onboard/activate",
                                                    StringArray,
                                                    onboard_mgr.handle_activate)
--- lg_keyboard/src/lg_keyboard/onboard.py
+++ lg_keyboard/src/lg_keyboard/onboard.py
@@ -8,9 +8,15 @@ import rospy
 from std_msgs.msg import String, Bool
 
 from lg_common import helpers
+from lg_common import ManagedApplication
+from lg_common import ManagedWindow
 
 
 ROS_NODE_NAME = "lg_onboard"
+
+
+class OnboardViewportException(Exception):
+    pass
 
 
 class OnboardManager(object):
@@ -19,18 +25,80 @@ class OnboardManager(object):
     the onboard system application.
 
     """
-    def __init__(self):
-        # list of lg_common.ManagedApplication
-        self.onboard_procs = []
-        # TODO
-        # init dbus as necessary
-        pass
+    def __init__(self, viewport=None, launcher=None):
+        self.viewport = viewport
+        self.launcher = launcher
+        if not self.viewport:
+            rospy.logerr("No viewport was configured for OnboardManager")
+        if not self.layncher:
+            rospy.logerr("No launcher was passed to OnboardManager")
 
-    def handle_activate(self, viewports):
-        # TODO
-        # 1) start onboards on target viewports
-        # 2) kill running onboards processes - upon reception of empty array StringArray([])
-        pass
+    def _show_keyboard(self):
+        self.launcher.show_onboard()
+
+    def _hide_keyboard(self):
+        self.launcher.hide_onboard()
+
+    def handle_activate(self, message):
+        """
+        Call show_keyboard to idempotently show kayboard
+        if is self.viewport was emitted on the list of
+        viewports in the incoming message.
+
+        Call hide_keyboard to idempotently close keyboard
+        if self.viewport was not emitted on the incoming list
+        """
+        if len(message.strings) > 0:
+            if self.viewport in message.string:
+                self._show_keyboard()
+            else:
+                self._hide_keyboard()
 
     def on_shutdown(self):
         rospy.loginfo("Received shutdown request.")
+
+
+class OnboardLauncher(object):
+    def __init__(self, config, viewport=None):
+        self.viewport = viewport
+        self.selected = False
+        self.app = None
+        self.window = None
+        self.config = config
+
+        cmd = ['/usr/bin/onboard', '-m']
+
+        if self.viewport is not None:
+            window = ManagedWindow()
+            geometry = self.ManagedWindow.lookup_viewport_geometry(self.viewport)
+
+            width = geometry.width
+            height = geometry.height / 4
+            x = geometry.x
+            y = geometry.y + geometry.height - height
+            cmd.extend(map(str, [
+                '-x', x,
+                '-y', y,
+                '-s', '{}x{}'.format(width, height)
+            ]))
+        else:
+            message = "Could not find viewport for OnboardLauncher - dying"
+            rospy.logerr(message)
+            raise OnboardViewportException(message)
+
+        self.app = ManagedApplication(cmd, window)
+
+    def show_onboard(self):
+        """
+        Idempotently shows onboard window
+        """
+        dconf = Popen(['/usr/bin/dconf', 'load', '/org/onboard/'], stdin=PIPE)
+        dconf.communicate(input=self.config)
+
+        self.app.set_state(ApplicationState.VISIBLE)
+
+    def hide_onboard(self):
+        """
+        Idemmpotently hides onboard window
+        """
+        self.app.set_state(ApplicationState.HIDDEN)
