e58dae87a54a2a51db4cc7daf0c9b876724e14d8
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-09-29 17:01:41 -0300

    Parse allowed urls config from get args

--- lg_common/src/lg_common/extensions/monitor_page_urls/blocker.js
+++ lg_common/src/lg_common/extensions/monitor_page_urls/blocker.js
@@ -1,20 +1,73 @@
 (function (){
 
+    function parseUrl(url) {
+        var parser = document.createElement('a');
+        parser.href = url;
+        return getQueryParams(parser.search);
+    }
+
+    function getQueryParams(qs) {
+        qs = qs.split('+').join(' ');
+        var params = {},
+            tokens,
+            re = /[?&]?([^=]+)=([^&]*)/g;
+        while (tokens = re.exec(qs)) {
+            var key = decodeURIComponent(tokens[1])
+            var val = decodeURIComponent(tokens[2]);
+            if (params[key] === undefined) {
+                params[key] = val;
+            }
+            else if ($.isArray(params[key])) {
+                params[key].push(val);
+            }
+            else {
+                params[key] = [params[key], val];
+            }
+        }
+        return params;
+    }
+
     var templates = [];
     var redirectUrl = null;
     var stats = new LgStats();
 
+    function setParamsAndProceed(url, callback) {
+        var conf = parseUrl(url);
+        redirectUrl = conf.redirect_to || url;
+        callback(conf.allowed_pages);
+    }
+
     function load(ready) {
-        $.get( "file:///opt/ep/allowed_pages.json", function( data ) {
-            console.log("Allowed pages: " + data);
-            var conf = JSON.parse(data);
-            if (conf) {
-                redirectUrl = conf.redirect_to;
-                ready(conf.allowed_pages);
+        // 1. get args
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+
+            if(tabs.length > 0 && tabs[0].url
+                && tabs[0].url.indexOf('allowed_urls') >= 0) {
+                setParamsAndProceed(tabs[0].url, ready);
             }
-            else {
-                console.log("FAILED to load allowed pages");
-            }
+
+            // 2. history
+            chrome.history.search({text: '', maxResults: 10}, function(histItems) {
+                for (var i = 0; i < histItems.length; i++) {
+                    var histItem = histItems[i];
+                    if (histItem.url && histItem.url.indexOf('allowed_urls') >= 0) {
+                        setParamsAndProceed(tabs[0].url, ready);
+                    }
+                }
+
+                // 3. config file
+                $.get( "file:///opt/ep/allowed_pages.json", function( data ) {
+                    console.log("Allowed pages: " + data);
+                    var conf = JSON.parse(data);
+                    if (conf) {
+                        redirectUrl = conf.redirect_to;
+                        ready(conf.allowed_pages);
+                    }
+                    else {
+                        console.log("FAILED to load allowed pages");
+                    }
+                });
+            });
         });
     }
 
@@ -83,10 +136,4 @@
     }
 */
 
-/*
-    chrome.webRequest.onBeforeRequest.addListener(callback,
-                                                  {urls: ["<all_urls>"]},
-                                                  ["blocking"]);
-    setInterval(checkTabs, 500);
-*/
 })();
