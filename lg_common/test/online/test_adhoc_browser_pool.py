f4a16414eb77221fab9880baf7dd98383eeb4c0a
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-10-02 22:45:50 -0300

    Tests for allowed urls message passing

--- lg_common/test/online/test_adhoc_browser_pool.py
+++ lg_common/test/online/test_adhoc_browser_pool.py
@@ -31,6 +31,20 @@ class TestAdhocBrowserPool(unittest.TestCase):
         assert 'rosbridge_secure=1' in url
         assert 'rosbridge_port=1234' in url
 
+    def test_allowed_url_params_passed(self):
+        test_browser_msg = AdhocBrowser()
+        test_browser_msg.allowed_urls.append('endpoint.com')
+        test_browser_msg.allowed_urls.append('google.com')
+        test_browser_msg.allowed_urls.append('test_url_pattern')
+
+        self.pool._create_browser('test_test', test_browser_msg)
+        assert self.pool.browsers['test_test'] is not None
+
+        url = self.pool.browsers['test_test'].url
+        assert '&allowed_urls=endpoint.com' in url
+        assert '&allowed_urls=google.com' in url
+        assert '&allowed_urls=test_url_pattern' in url
+
 if __name__ == '__main__':
     rospy.init_node(NAME)
     import rostest
