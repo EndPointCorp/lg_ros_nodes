bbc23a8606f3300ee331bd8ee54449106f5dd3a9
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-10-12 00:23:34 -0300

    Parse string and obj extensions in director bridge

--- lg_common/src/lg_common/adhoc_browser_director_bridge.py
+++ lg_common/src/lg_common/adhoc_browser_director_bridge.py
@@ -123,7 +123,10 @@ class AdhocBrowserDirectorBridge():
         if extensions:
             for extension in extensions:
                 browser_extension = BrowserExtension()
-                browser_extension.name = str(extension['name'])
+                if isinstance(extension, basestring):
+                    browser_extension.name = str(extension)
+                else:
+                    browser_extension.name = str(extension['name'])
                 adhoc_browser.extensions.append(browser_extension)
 
         if allowed_urls:
