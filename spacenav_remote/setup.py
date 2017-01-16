b4fd549f8a413fc53d292fa961f9de754f1e1b19
Galaxy Admin <galadmin@lg-head-307.liquid.glx>
2017-01-16 18:17:25 -0500

    Fix setup.py

--- spacenav_remote/setup.py
+++ spacenav_remote/setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
+    packages=['spacenav_remote'],
+    package_dir={'': 'src'},
 )
 
 setup(**d)
