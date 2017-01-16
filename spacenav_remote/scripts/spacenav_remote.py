11edeac3aa9a015ac4c2ab401ea139a4b625a10a
kiselev-dv <dmitry.v.kiselev@gmail.com>
2017-01-16 17:55:20 -0400

    Fix circular dependancy

--- spacenav_remote/scripts/spacenav_remote.py
+++ spacenav_remote/scripts/spacenav_remote.py
@@ -5,7 +5,7 @@ import rospy
 
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
-from spacenav_remote import SpacenavRemote
+import SpacenavRemote
 
 NODE_NAME = 'spacenav_remote'
 
