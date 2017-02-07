d4c02537dc815a71acc8f1089119b9489ed94801
Will Plaut <will@endpoint.com>
2017-02-07 20:26:15 +0000

    fixed up the lg_rfreceiver device reading technique

--- lg_rfreceiver/scripts/sender.py
+++ lg_rfreceiver/scripts/sender.py
@@ -22,41 +22,31 @@ def main():
     device_path = rospy.get_param('~device_path')
     baud_rate = rospy.get_param('~baud_rate', 9600)
     retry_grace_time = rospy.get_param('~retry_grace_time', 30)
-    retry_attempts = rospy.get_param('~retry_attempts', 5)
+
 
     ready = False
-
-    # Try `retry_attempts` times with `retry_grace_time` grace period between attempts to open the device
-
-    while not ready and retry_attempts >= 0:
+    i = 0
+    while not ready:
         try:
             receiver = serial.Serial(port=device_path, baudrate=baud_rate)
             ready = True
         except SerialException:
             rospy.logerr("Could not open device_path: %s" % (device_path))
-            rospy.logerr("I'm going to try %s times more and will sleep %s seconds between attempts" % (retry_attempts, retry_grace_time))
-            time.sleep(retry_grace_time)
-            retry_attempts -= 1
-
-    if retry_attempts <= 0:
-        rospy.logerr("Could not attach to serial device %s, sleeping for %s secs" % (device_path, retry_grace_time * 100))
-        rospy.sleep(retry_grace_time * 100)
+            rospy.logerr("I'm going to sleep %s seconds between attempts" % (i))
+            time.sleep(i)
+            if i < retry_grace_time:
+                i += 3
 
     buf = ''
 
-    if not ready:
-        rospy.logerr("Giving up - couldn't connect to the device %s" % device_path)
-        while not rospy.is_shutdown():
-            pass
-    else:
-        while not rospy.is_shutdown():
-            try:
-                button = int(receiver.readline(10).strip())
-            except serial.SerialException as e:
-                print e
-                break
+    while not rospy.is_shutdown():
+        try:
+            button = int(receiver.readline(10).strip())
+        except serial.SerialException as e:
+            print e
+            break
 
-            buttondown_pub.publish(Byte(button))
+        buttondown_pub.publish(Byte(button))
 
 if __name__ == '__main__':
     run_with_influx_exception_handler(main, NODE_NAME)
