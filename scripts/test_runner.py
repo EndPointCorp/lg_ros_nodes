e671c0d0b95109cc39c5885c3c65d0a422b01975
Matt Vollrath <matt@endpoint.com>
2017-03-09 17:53:44 -0500

    Don't run gjslint against panovideosync
    The deprecated closure linter can't check ES6.
    
    See #342

--- scripts/test_runner.py
+++ scripts/test_runner.py
@@ -91,7 +91,7 @@ def cppcheck_test():
 
 
 def gjslint_test():
-    ret = os.system("gjslint --nojsdoc --max_line_length 120 --disable 0001,0002,0131,0120 -e 'lib,lg_cms_director' -r .")
+    ret = os.system("gjslint --nojsdoc --max_line_length 120 --disable 0001,0002,0131,0120 -e 'lib,lg_cms_director,panovideosync' -r .")
     return ret
 
 
