8a1a15d2a90fbf7f99a9c7e624c217fe649d82c6
kiselev-dv <dmitry.v.kiselev@gmail.com>
2017-06-12 11:02:45 -0300

    Remove empty first line in xml template

--- scripts/pose_to_kml.py
+++ scripts/pose_to_kml.py
@@ -2,8 +2,7 @@ import sys
 import csv
 from math import sin, radians, fabs
 
-KML_TEMPLATE = """
-<?xml version="1.0" encoding="UTF-8"?>
+KML_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" 
     xmlns:kml="http://www.opengis.net/kml/2.2" 
     xmlns:atom="http://www.w3.org/2005/Atom">
