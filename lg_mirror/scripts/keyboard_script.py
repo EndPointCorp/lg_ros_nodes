fd0be42acce4e13adc0938c57c978eb293030feb
Jacob Minshall <jacob@endpoint.com>
2018-04-02 18:31:22 -0400

    fixing up logerrs

--- lg_mirror/scripts/keyboard_script.py
+++ lg_mirror/scripts/keyboard_script.py
@@ -21,7 +21,6 @@ class KeyboardThing:
         for event in msg.events:
             if event.type != 1:
                 continue
-            rospy.logerr("Writing event...")
             self.ui.write(event.type, event.code, event.value)
 
     def handle_route_change(self, msg):
@@ -29,7 +28,6 @@ class KeyboardThing:
         for route in self.current_routes:
             if route in self.routes:
                 self.active = True
-                rospy.logerr("We are active...")
                 return
         self.active = False
 
@@ -40,10 +38,9 @@ def main():
     routes = rospy.get_param('~routes', '')
     routes = routes.split(',')
 
-    rospy.logerr("Starting UP THIS NOOOOOODE")
     kb = KeyboardThing(routes)
 
-    rospy.logerr("subscribing to %s and %s with routes %s" % (event_topic + 'active_routes', event_topic + 'events', routes))
+    rospy.logdebug("subscribing to %s and %s with routes %s" % (event_topic + 'active_routes', event_topic + 'events', routes))
     rospy.Subscriber(event_topic + 'active_routes', StringArray, kb.handle_route_change)
     rospy.Subscriber(event_topic + 'events', EvdevEvents, kb.handle_event)
 
