3d87af04c0e8526292a0f24de140db7af4fa88d9
Will Plaut <will@endpoint.com>
2018-05-01 17:30:48 -0400

    double clicks

--- lg_mirror/scripts/keyboard_script.py
+++ lg_mirror/scripts/keyboard_script.py
@@ -7,20 +7,29 @@ from lg_common.msg import StringArray
 
 
 class KeyboardThing:
-    def __init__(self, routes=None):
+    def __init__(self, routes=None, double_click=-1):
         self.current_route = None
         self.routes = routes
         self.active = False
+        self.double_click = int(double_click)
         if self.routes is None:
             self.routes = []
         self.ui = evdev.UInput()
 
     def handle_event(self, msg):
         for event in msg.events:
+            rospy.logerr("handling button {}".format(event))
             if event.type == 3:
                 continue
             if self.active or event.value == 0:  # always publishing key-ups hopefully this doesn't come back to haunt us because honestly it definitely could but thats not something im going to worry about now...
-                self.ui.write(event.type, event.code, event.value)
+                if event.code == self.double_click and event.value == 1:
+                    rospy.logerr("foo ZZZ")
+                    self.ui.write(1,272,1)
+                    self.ui.write(1,272,0)
+                    self.ui.write(1,272,1)
+                    self.ui.write(1,272,0)
+                else:
+                    self.ui.write(event.type, event.code, event.value)
 
         self.ui.syn()
 
@@ -37,11 +46,12 @@ def main():
 
     event_topic = rospy.get_param('~event_topic', '/lg_mirror/logitech/')
     routes = rospy.get_param('~routes', '')
+    double_click = rospy.get_param('~double_click_key_code', -1)
     routes = routes.split(',')
 
-    kb = KeyboardThing(routes)
+    kb = KeyboardThing(routes, double_click)
 
-    rospy.logdebug("subscribing to %s and %s with routes %s" % (event_topic + 'active_routes', event_topic + 'kbd_events', routes))
+    rospy.logdebug("subscribing to %s and %s with routes %s" % (event_topic + 'active_routes', event_topic + 'events', routes))
     rospy.Subscriber(event_topic + 'active_routes', StringArray, kb.handle_route_change)
     rospy.Subscriber(event_topic + 'kbd_events', EvdevEvents, kb.handle_event)
 
