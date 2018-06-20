8f1450229f28c0aa204a3f6efbb3910be5bb8d86
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2018-06-20 16:55:54 -0300

    Load google maps API dynamically

---
+++ lg_sv/webapps/client/js/mapsAPI.js
@@ -0,0 +1,23 @@
+// Load some scripts in the right order
+
+// <script src="http://maps.googleapis.com/maps/api/js?signed_in=true"></script>
+// <script src="js/main.js"></script>
+
+(function() {
+    var map_key = getParameterByName('map_key', String, '');
+    var src = 'http://maps.googleapis.com/maps/api/js?key=' + map_key;
+
+    load(src, function() {
+        load('js/main.js');
+    });
+
+    function load(src, onload) {
+        var script = document.createElement('script');
+        if (onload) {
+            script.onload = onload;
+        }
+        script.src = src;
+        document.head.appendChild(script);
+    }
+
+})();
