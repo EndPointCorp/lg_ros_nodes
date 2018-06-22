4c67ef5100a161bd4385bc66d8d9551881cd5025
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2018-06-22 15:37:47 -0300

    Log map api key

--- lg_sv/scripts/launcher.py
+++ lg_sv/scripts/launcher.py
@@ -59,6 +59,8 @@ def main():
                          rosbridgePort=rosbridge_port,
                          rosbridgeSecure=rosbridge_secure)
 
+    rospy.loginfo('Use Google maps API key: {}'.format(map_api_key))
+    
     if map_api_key:
         url = add_url_params(url, map_api_key=map_api_key)
 
