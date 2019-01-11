de8a8fdcd6b7c086440218f6163930f61ab55b73
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2019-01-11 15:30:57 -0400

    Move Locker service to src

--- lg_lock/scripts/service.py
+++ lg_lock/scripts/service.py
@@ -2,47 +2,11 @@
 
  from lg_lock.srv import IsLocked, Lock, UnLock
  from lg_lock.msg import State
+ from lg_lock import LockerService
  import rospy
  from lg_common.helpers import run_with_influx_exception_handler
 
  NODE_NAME = 'lg_lock'
-
-class Locker(object):
-    """
-    Lock/Unlock LG
-
-    param password - password to unlock LG
-    param state - Bool initial state
-    """
-    def __init__(self, statePublisher, password, state):
-        self.password = password
-        self.state = state
-        self.publisher = statePublisher
-        self.publishState()
-
-    def lock(self, msg):
-        self._lock()
-        return self.state
-
-    def unlock(self, msg):
-        if (self.password == msg.password) {
-            self._unlock()
-        }
-        return self.state
-
-    def get_state(self, msg):
-        return self.state
-
-    def _unlock(self):
-        self.state = False
-        self.publishState()
-    
-    def _lock(self):
-        self.state = True   
-        self.publishState()
-
-    def publishState(self):
-        self.publisher.publish(State(self.state))
 
  def init():
     rospy.init_node(NODE_NAME, anonymous=False)
@@ -56,7 +20,7 @@ class Locker(object):
         rospy.logerr('No or blank password provided, exiting...')
         return
 
-    service = Locker(statePublisher, password, locked)
+    service = LockerService(statePublisher, password, locked)
 
     rospy.Service('is_locked', IsLocked, service.get_state)
     rospy.Service('lock', Lock, activity_tracker.lock)
---
+++ lg_lock/src/lg_lock/locker_service.py
@@ -0,0 +1,36 @@
+class LockerService(object):
+    """
+    Lock/Unlock LG
+
+    param password - password to unlock LG
+    param state - Bool initial state
+    """
+    def __init__(self, statePublisher, password, state):
+        self.password = password
+        self.state = state
+        self.publisher = statePublisher
+        self.publishState()
+
+    def lock(self, msg):
+        self._lock()
+        return self.state
+
+    def unlock(self, msg):
+        if (self.password == msg.password) {
+            self._unlock()
+        }
+        return self.state
+
+    def get_state(self, msg):
+        return self.state
+
+    def _unlock(self):
+        self.state = False
+        self.publishState()
+    
+    def _lock(self):
+        self.state = True   
+        self.publishState()
+
+    def publishState(self):
+        self.publisher.publish(State(self.state))
