00f41fa55f5dd157e4371f7400bf8213ddc78f6a
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2019-01-11 22:02:24 -0400

    add some debug output

--- spacenav_wrapper/scripts/spacenav_wrap.py
+++ spacenav_wrapper/scripts/spacenav_wrap.py
@@ -33,6 +33,7 @@ def main():
 
     s = SpacenavWrapper(twist=twist, joy=joy, gutter_val=gutter_val)
     def suppress(msg):
+        rospy.loginfo('Suppress spacenav output: {}'.format(msg.data))
         s.suppress(msg.data)
         return SetBoolResponse(True, "")
     rospy.Service('/spacenav_wrapper/suppress', SetBool, suppress)
