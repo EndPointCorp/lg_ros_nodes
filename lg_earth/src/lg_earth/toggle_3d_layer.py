3b239e26a137128a1cc6663c3b13eb1d02198119
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2019-01-28 23:20:05 -0400

    Refactored 3d layer set service

--- lg_earth/src/lg_earth/toggle_3d_layer.py
+++ lg_earth/src/lg_earth/toggle_3d_layer.py
@@ -6,10 +6,13 @@ class Toggle3d:
     
     def set_layer_state(self, state):
         self.sync_state_to_earth(state)
-        self.layer_is_on = state  
+        self.layer_is_on = state
+        
+        if self.state_listener:
+            self.state_listener(self.layer_is_on)
 
-    def get_state(self):
-        return self.layer_is_on
+    def set_on_change_listener(self, state_listener):
+        self.state_listener = state_listener
 
     def sync_state_to_earth(self, state):
         subprocess.call(['/home/lg/bash_scripts/toggle_layer.bash', 'on' if state else 'off'])
