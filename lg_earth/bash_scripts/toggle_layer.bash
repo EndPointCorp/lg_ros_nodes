1431bbc1f779c810eafc3fd8fbc77f828c2343e9
Galaxy Admin <galadmin@lg-head-131.liquid.glx>
2019-02-14 13:33:17 -0500

    TOGGLE: adjusted script

--- lg_earth/bash_scripts/toggle_layer.bash
+++ lg_earth/bash_scripts/toggle_layer.bash
@@ -7,7 +7,7 @@ fi
 # Desired layer state
 BUILDING_LAYER="$1"
 # Earth Instance IDs
-EARTH_INSTANCES=$(export DISPLAY=:0 ; xdotool search --name "Earth")
+EARTH_INSTANCES=$(export DISPLAY=:0 ; xdotool search --name "Earth EC")
 # Track if Errors Present
 ERRORS_PRESENT=false
 
@@ -27,46 +27,38 @@ disable_keyboard () {
 toggle_menu () {
         export DISPLAY=:0
         xdotool windowactivate $1
-        xdotool key  ctrl+alt+b
-}
-
-# Toggle 3D Building Layer
-toggle_layer () {
-    for i in $EARTH_INSTANCES
-        do
-            export DISPLAY=:0
-            xdotool windowactivate $i
-			for i in {1..6}
+		if [ $2 == "on" ]
+		then
+			xdotool key  ctrl+alt+b
+			for i in {1..7}
 			do
-				xdotool key --window $i Tab
+				xdotool key Tab
 			done
-			for i in {1..6}
-			do 
-				xdotool key --window $i Down
-			done
-			xdotool key --window $i space
-			for i in {1..6}
-			do 
-				xdotool key --window $i Up
-			done
-        done
-}
+			xdotool key space
+		else
+			sleep .1
+			xdotool key Tab
+			xdotool key space
+			xdotool key  ctrl+alt+b
+		fi
+} 
 
 # Capture Current Layer State
 capture_current_state ()
 {
+	echo "CAPPING CURRENT STATE"
     for i in $EARTH_INSTANCES
     do
         export DISPLAY=:0
         xwd -display :0 -id $i | convert xwd:- png:- > ~/tmp/42-a_"$i".png
-        convert ~/tmp/42-a_"$i".png -crop 5x5+33+1151 ~/tmp/42-a_"$i".png
+        convert ~/tmp/42-a_"$i".png -crop 5x5+35+1114 ~/tmp/42-a_"$i".png
         convert ~/tmp/42-a_"$i".png txt:- > ~/tmp/42-a_"$i"
     done
 }
 
 # Validate 3D Layer On
 validate_on () {
-
+	echo "VALIDATING ON"
     for i in $EARTH_INSTANCES
     do
         cmp --silent /home/lg/bash_scripts/3d_layer_off ~/tmp/42-a_"$i"
@@ -81,6 +73,7 @@ validate_on () {
 
 # Validate 3D Layer Off
 validate_off () {
+	echo "VALIDATING OFF"
     for i in $EARTH_INSTANCES
     do
         cmp --silent  /home/lg/bash_scripts/3d_layer_on_1 ~/tmp/42-a_"$i"
@@ -127,24 +120,27 @@ fix_errors() {
             export DISPLAY=:0
             xdotool windowactivate $i
             sleep .1
-            for i in {1..6}
-            do
-                xdotool key Tab
-            done
-			sleep .1
 			for i in {1..6}
+			do
+				xdotool key Tab
+				sleep .1
+			done
+			sleep .1
+			for i in {1..7}
             do
                 xdotool key Down
             done
 			sleep .1
             xdotool key space
 			sleep .1
-            for i in {1..6}
+            for i in {1..7}
             do
                 xdotool key Up
             done
+			sleep .1
         done
     fi
+	sleep .3
     unset EARTH_ERROR
     EARTH_ERROR=()
     ERRORS_PRESENT=false
@@ -153,18 +149,20 @@ fix_errors() {
 }
 
 validate_closed () {
-
-	capture_current_state	
+	export DISPLAY=:0
+	# Capture point on menu that is white and compare
 	for i in EARTH_INSTANCES
 	do
-		cmp --silent  /home/lg/bash_scripts/3d_layer_on_1 ~/tmp/42-a_"$i"
-        result1=$?
-        cmp --silent  /home/lg/bash_scripts/3d_layer_on_2 ~/tmp/42-a_"$i"
-        result2=$?	
-		cmp --silent /home/lg/bash_scripts/3d_layer_off ~/tmp/42-a_"$i"
-		result3=$?
-		if [ $result1 == 0 ] || [ $result2 == 0 ] || [ $result3 == 0 ]; then
-			toggle_menu $i
+		xdotool windowactivate $i
+		xwd -display :0 -id $i | convert xwd:- png:- > ~/tmp/42-a_blank.png
+		convert ~/tmp/42-a_blank.png -crop 15x15+120x760 ~/tmp/42-a_blank.png
+		convert ~/tmp/42-a_blank.png txt:- > ~/tmp/42-a_blank
+		cmp --silent  /home/lg/bash_scripts/menu_open ~/tmp/42-a_blank
+        result=$?
+		if [ $result == 0 ] ; then
+			export DISPLAY=:0
+			xdotool windowactivate $i
+			xdotool key ctrl+alt+b
 		fi
 	done
 }
@@ -172,7 +170,8 @@ validate_closed () {
 disable_keyboard "unbind"
 for i in $EARTH_INSTANCES
 do
-	toggle_menu $i
+	sleep .1
+	toggle_menu $i "on"
 done
 if [ "$BUILDING_LAYER" == on ]
 then
@@ -182,13 +181,13 @@ else
 fi
 for i in $EARTH_INSTANCES
 do
-    toggle_menu $i
+    toggle_menu $i "off"
 done
 validate_closed
-if [ $HOSTNAME == "42-a" ]
+if [ $HOSTNAME == "42-b" ]
 then
-	sleep .5
 	python /home/lg/bash_scripts/repub.py
 fi
 rm ~/tmp/42-a*
 disable_keyboard "bind"
+
