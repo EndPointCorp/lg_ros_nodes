aaf628216ef4b565f14f1219216565d883414834
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2019-03-19 18:38:07 -0300

    application to listen on add_kml topics

--- lg_earth/add_kml_snippet.js
+++
@@ -1,9 +0,0 @@
-let addedKMLTopic = new ROSLIB.Topic({
-    ros: ros,
-    name: 'lg_earth/added_kml',
-    messageType: 'lg_common/StringArray'
-});
-
-addedKMLTopic.subscribe(function(msg){
-    msg.strings // ids of added kmls
-});
