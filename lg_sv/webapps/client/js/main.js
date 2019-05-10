ad487d2b82ed406880093e1871b4680765692a83
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2019-05-10 13:58:09 -0300

    reinitialize on ros close

--- lg_sv/webapps/client/js/main.js
+++ lg_sv/webapps/client/js/main.js
@@ -37,6 +37,10 @@ var initializeViewers = function() {
   ros.on('error', function() {
     setTimeout(initialize, 2000);
   });
+  ros.on('close', function() {
+    console.error('Lost ROS connection');
+    setTimeout(initialize, 1000);
+  });
 };
 
 var initializeRes = function(ros, yawOffset) {
