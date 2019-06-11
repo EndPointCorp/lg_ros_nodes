a0f57eec38da666a76ff18de37576227d4019549
Will Plaut <will@endpoint.com>
2019-06-11 14:09:11 -0400

    more changes

--- lg_earth/src/lg_earth/kmlalive.py
+++ lg_earth/src/lg_earth/kmlalive.py
@@ -5,15 +5,19 @@ import rosservice
 class KmlAlive:
     def __init__(self, earth_proc):
         self.earth_proc = earth_proc
-        self.pid = earth_proc.proc.watcher.proc.pid
         rospy.sleep(5)
         self.keep_alive()
 
     def keep_alive(self):
+        loop_timeout = 0.2
         with open('/dev/null', 'w') as dev_null:
             while not rospy.is_shutdown():
+                try:
+                    pid = self.earth_proc.proc.watcher.proc.pid
+                except AttributeError:
+                    rospy.sleep(loop_timeout)
                 if '/kmlsync/state' in rosservice.get_service_list():
-                    cmd = "lsof -Pn -p {} -a -i @127.0.0.1:8765".format(self.pid).split(' ')
+                    cmd = "lsof -Pn -p {} -a -i @127.0.0.1:8765".format(pid).split(' ')
                         ret_value = subprocess.call(
                             cmd,
                             stdout=dev_null,
@@ -22,4 +26,4 @@ class KmlAlive:
                         )
                         if ret_value != 0:
                             self.earth_proc.handle_soft_relaunch()
-               rospy.sleep(0.2)
+               rospy.sleep(loop_timeout)
