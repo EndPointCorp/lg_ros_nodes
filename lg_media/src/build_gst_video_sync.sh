4930cca430e7338b08cfc87cfda0bda9f8ec7d66
Matt Vollrath <matt@endpoint.com>
2019-06-29 02:22:50 -0400

    GStreamer-based sync video player

---
+++ lg_media/src/build_gst_video_sync.sh
@@ -0,0 +1,6 @@
+g++ -fPIC -Wall gst_video_sync.cpp -o gst_video_sync \
+  -DG_LOG_DOMAIN="\"app\"" \
+  $(pkg-config --cflags --libs gstreamer-1.0) \
+  $(pkg-config --cflags --libs gstreamer-video-1.0) \
+  $(pkg-config --cflags --libs gstreamer-plugins-base-1.0) \
+  $(pkg-config --cflags --libs Qt5GStreamerUi-1.0)
