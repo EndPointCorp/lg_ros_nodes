767779476de80a553bed58768dccfc4d376063aa
Matt Vollrath <matt@endpoint.com>
2019-07-01 17:45:36 -0700

    gst_video_sync: Disable pausing while buffering
    Does not improve the experience.

--- lg_media/src/gst_video_sync.cpp
+++ lg_media/src/gst_video_sync.cpp
@@ -249,11 +249,13 @@ gboolean SyncVideoApp::bus_callback(GstBus *bus, GstMessage *msg) {
       gint percent = 0;
       gst_message_parse_buffering(msg, &percent);
       g_debug("buffering at %u percent\n", percent);
+      /*
       if (percent < 100) {
         gst_element_set_state(this->player, GST_STATE_PAUSED);
       } else {
         gst_element_set_state(this->player, GST_STATE_PLAYING);
       }
+      */
       break;
     }
     default:
