f8257167481d7e1267122cc5dc3e48343696b464
Matt Vollrath <matt@endpoint.com>
2019-11-07 21:31:28 -0500

    Fix onboard tests

--- lg_keyboard/test/online/test_onboard_router.py
+++ lg_keyboard/test/online/test_onboard_router.py
@@ -3,14 +3,10 @@
 PKG = 'lg_keyboard'
 NAME = 'test_onboard_router'
 
-import os
 import time
 import unittest
 
-import pytest
 import rospy
-import rospkg
-from multiprocessing import Array
 
 from lg_common.msg import StringArray
 from std_msgs.msg import Bool
@@ -21,61 +17,21 @@ from lg_common.test_helpers import gen_scene
 from lg_common.test_helpers import gen_scene_msg
 
 
-SCENE = Array('c', 1000)  # size
-VISIBILITY = Array('c', 1000)  # size
-ACTIVATE = Array('c', 1000)  # size
-
-
 class TestOnboardRouterOnline(unittest.TestCase):
     def setUp(self):
-        rospy.Subscriber(
-            '/lg_onboard/visibility',
-            Bool,
-            self.callback_visibility_receiver
-        )
-        rospy.Subscriber(
+        self.activates = []
+        self.activates_sub = rospy.Subscriber(
             '/lg_onboard/activate',
             StringArray,
-            self.callback_activate_receiver
-        )
-        rospy.Subscriber(
-            '/director/scene',
-            GenericMessage,
-            self.callback_scene_receiver
+            self.activates.append
         )
         self.director_publisher = rospy.Publisher('/director/scene', GenericMessage, queue_size=10)
         self.visibility_publisher = rospy.Publisher('/lg_onboard/visibility', Bool, queue_size=10)
-        SCENE.value = b"UNDEFINED"
-        VISIBILITY.value = b"UNDEFINED"
-        ACTIVATE.value = b"UNDEFINED"
-        # must be after pubs/subs initialization:
-        self.grace_delay = 3
-        rospy.sleep(self.grace_delay)
+        rospy.sleep(1)
 
     def tearDown(self):
+        self.activates_sub.unregister()
         time.sleep(1)
-
-    @staticmethod
-    def callback_scene_receiver(msg):
-        rospy.loginfo("callback received type: '%s', message: %s" % (type(msg), msg))
-        SCENE.value = str(msg.message).encode('utf-8')
-
-    @staticmethod
-    def callback_visibility_receiver(msg):
-        rospy.loginfo("callback received type: '%s', message: %s" % (type(msg), msg))
-        VISIBILITY.value = str(msg.data).encode('utf-8')
-
-    @staticmethod
-    def callback_activate_receiver(msg):
-        rospy.loginfo("callback received type: '%s', message: %s" % (type(msg), msg))
-        ACTIVATE.value = str(msg.strings).encode('utf-8')
-
-    def active_wait(self, what, where, timeout=5):
-        for _ in range(timeout):
-            rospy.sleep(1)
-            if where == what.encode('utf-8'):
-                break
-        assert where == what.encode('utf-8')
 
     def test_1_sending_messages_work(self):
         msg = GenericMessage(type='json', message='{}')
@@ -83,9 +39,7 @@ class TestOnboardRouterOnline(unittest.TestCase):
         time.sleep(1)
         self.visibility_publisher.publish(Bool(data=True))
         time.sleep(1)
-        self.active_wait('{}', SCENE.value)
-        self.active_wait('True', VISIBILITY.value)
-        self.active_wait("['kiosk']", ACTIVATE.value)
+        self.assertEqual('kiosk', self.activates[-1].strings[0])
 
     def test_2_default_viewport_no_route_touch(self):
         """
@@ -105,8 +59,7 @@ class TestOnboardRouterOnline(unittest.TestCase):
         self.visibility_publisher.publish(Bool(data=False))
         self.visibility_publisher.publish(Bool(data=True))
         time.sleep(1)
-        self.active_wait('True', VISIBILITY.value)
-        self.active_wait("['kiosk']", ACTIVATE.value)
+        self.assertEqual('kiosk', self.activates[-1].strings[0])
 
     def test_3_default_viewport_no_route_touch(self):
         """
@@ -127,8 +80,7 @@ class TestOnboardRouterOnline(unittest.TestCase):
         self.visibility_publisher.publish(Bool(data=False))
         self.visibility_publisher.publish(Bool(data=True))
         time.sleep(1)
-        self.active_wait('True', VISIBILITY.value)
-        self.active_wait("['kiosk']", ACTIVATE.value)
+        self.assertEqual('kiosk', self.activates[-1].strings[0])
 
     def test_4_route_touch_on_one_viewport(self):
         """
@@ -156,8 +108,7 @@ class TestOnboardRouterOnline(unittest.TestCase):
         self.visibility_publisher.publish(Bool(data=False))
         self.visibility_publisher.publish(Bool(data=True))
         time.sleep(1)
-        self.active_wait('True', VISIBILITY.value)
-        self.active_wait("['cthulhu_fhtagn']", ACTIVATE.value)
+        self.assertEqual('cthulhu_fhtagn', self.activates[-1].strings[0])
 
     def test_5_route_touch_on_two_viewports(self):
         """
@@ -185,8 +136,8 @@ class TestOnboardRouterOnline(unittest.TestCase):
         self.visibility_publisher.publish(Bool(data=False))
         self.visibility_publisher.publish(Bool(data=True))
         time.sleep(1)
-        self.active_wait('True', VISIBILITY.value)
-        self.active_wait("['cthulhu_fhtagn', 'iah_iah']", ACTIVATE.value)
+        self.assertTrue('cthulhu_fhtagn' in self.activates[-1].strings)
+        self.assertTrue('iah_iah' in self.activates[-1].strings)
 
 
 if __name__ == '__main__':
