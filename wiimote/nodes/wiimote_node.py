6ad4a32eb318d3d205445c5990cb5dc19317b590
Will Plaut <will@endpoint.com>
2019-11-11 12:56:12 -0500

    wiimote: fix broken import

--- wiimote/nodes/wiimote_node.py
+++ wiimote/nodes/wiimote_node.py
@@ -84,8 +84,8 @@ from std_msgs.msg import Bool
 from sensor_msgs.msg import Joy
 from sensor_msgs.msg import JoyFeedback
 from sensor_msgs.msg import JoyFeedbackArray
-from wiimote.msg import IrSourceInfo
-from wiimote.msg import State
+from lg_msg_defs.msg import IrSourceInfo
+from lg_msg_defs.msg import State
 
 # -------- WIIMote Modules:
 from wiimote.wiimoteExceptions import *
