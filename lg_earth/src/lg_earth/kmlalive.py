016666f93a94919a423101288d68ca2f31a90fc3
Matt Vollrath <matt@endpoint.com>
2020-01-13 17:08:20 -0500

    Clean up KmlAlive
    * Use subprocess.DEVNULL
    * Don't recurse on error

--- lg_earth/src/lg_earth/kmlalive.py
+++ lg_earth/src/lg_earth/kmlalive.py
@@ -16,43 +16,34 @@ class KmlAlive:
         self.worked = False
 
     def keep_alive(self, *args, **kwargs):
-        try:
-            self._keep_alive(args, kwargs)
-        except Exception as e:
-            rospy.logerr("exception was {} {} {}".format(e, traceback.format_exc(), sys.exc_info()[0]))
-            rospy.sleep(1)
-            self.keep_alive(args, kwargs)
-
-    def _keep_alive(self, *args, **kwargs):
         rospy.logerr("XXX in first keep_alive")
         loop_timeout = 1
         counter = 0
         rospy.sleep(1)
-        with open('/dev/null', 'w') as dev_null:
-            while not rospy.is_shutdown():
-                try:
-                    pid = self.earth_proc.proc.watcher.proc.pid
-                except AttributeError as e:
-                    counter = 0
-                    rospy.logwarn("Earth proc doesn't exist {}".format(e))
-                    rospy.sleep(loop_timeout)
-                    continue
-                cmd = "lsof -Pn -p {} -a -i @127.0.0.1:8765".format(pid).split(' ')
-                ret_value = subprocess.call(
-                    cmd,
-                    stdout=dev_null,
-                    stderr=dev_null,
-                    close_fds=True
-                )
-                if ret_value == 0:
-                    self.worked = True
-                    counter = 0
-                else:
-                    counter += 1
-                    rospy.logerr("XXX found non zero value for {} counter at {}".format(pid, counter))
-                    if (counter > self.timeout_period and self.worked) or counter > self.initial_timeout:
-                        rospy.logerr("XXX RELAUNCHING worked: {}  counter: {}".format(self.worked, counter))
-                        self.earth_proc.handle_soft_relaunch()
-                        counter = 0
-                        self.worked = False
+        while not rospy.is_shutdown():
+            try:
+                pid = self.earth_proc.proc.watcher.proc.pid
+            except AttributeError as e:
+                counter = 0
+                rospy.logwarn("Earth proc doesn't exist {}".format(e))
                 rospy.sleep(loop_timeout)
+                continue
+            cmd = "lsof -Pn -p {} -a -i @127.0.0.1:8765".format(pid).split(' ')
+            ret_value = subprocess.call(
+                cmd,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                close_fds=True
+            )
+            if ret_value == 0:
+                self.worked = True
+                counter = 0
+            else:
+                counter += 1
+                rospy.logerr("XXX found non zero value for {} counter at {}".format(pid, counter))
+                if (counter > self.timeout_period and self.worked) or counter > self.initial_timeout:
+                    rospy.logerr("XXX RELAUNCHING worked: {}  counter: {}".format(self.worked, counter))
+                    self.earth_proc.handle_soft_relaunch()
+                    counter = 0
+                    self.worked = False
+            rospy.sleep(loop_timeout)
