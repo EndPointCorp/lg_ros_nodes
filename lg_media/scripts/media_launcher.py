1372abc712509c4fca17743df92a546226e875b8
Galaxy Admin <galadmin@lg-head-300.liquid.glx>
2020-04-27 17:02:48 -0400

    Use ffplay in media launcher

--- lg_media/scripts/media_launcher.py
+++ lg_media/scripts/media_launcher.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 
-import json
 from collections import namedtuple
 
 import rospy
-from lg_common import ManagedWindow
+from lg_common import ManagedApplication, ManagedWindow
 from interactivespaces_msgs.msg import GenericMessage
-from lg_msg_defs.msg import MediaOverlays
+from lg_msg_defs.msg import ApplicationState, MediaOverlays
 
-StreamInfo = namedtuple('overlay', ['name', 'viewport', 'location', 'url'])
+StreamInfo = namedtuple('overlay', ['name', 'viewport', 'location'])
+
+STREAMS = {
+    "0": "10.42.41.151",
+    "1": "10.42.41.152"
+}
+
+class Player(ManagedApplication):
+    def __init__(self, stream_info, respawn=True):
+        window_id = ".".join(stream_info)
+        geometry = ManagedWindow.lookup_viewport_geometry(stream_info.viewport)
+        window = ManagedWindow(w_name=window_id, geometry=geometry)
+
+        cmd = f"/usr/bin/ffplay -nostats -rtsp_transport udp -fflags nobuffer -flags low_delay " \
+              f"-framedrop -analyzeduration 0 -noborder " \
+              f"-window_title \'{window_id}\' rtsp://{STREAMS[stream_info.location]}:554/default" \
+              .split()
+
+        rospy.logdebug(f"Starting new ManagedApplication for {stream_info}")
+
+        super(Player, self).__init__(cmd=cmd, window=window, respawn=respawn)
+
 
 class MediaLauncher(object):
     """Maintain a list of active streams based on MediaOverlays messages"""
-    def __init__(self, janus_host, janus_port, pub, viewports):
-        self.director_pub = pub
-        self.janus_host = janus_host
-        self.janus_port = janus_port
+    def __init__(self, viewports):
         self.viewports = viewports
 
         self.active_overlays = {}
@@ -27,7 +44,10 @@ class MediaLauncher(object):
         """
         message_overlays = []
         for msg in message.overlays:
-            message_overlays.append(StreamInfo(msg.name, msg.viewport, msg.location, self._make_url(msg.location)))
+            if msg.viewport in self.viewports:
+                message_overlays.append(StreamInfo(msg.name, msg.viewport, msg.location))
+            else:
+                rospy.logdebug("Pass")
 
         stale_overlays = set(self.active_overlays.keys()).difference(set(message_overlays))
         new_overlays = set(message_overlays).difference(set(self.active_overlays.keys()))
@@ -41,70 +61,35 @@ class MediaLauncher(object):
 
         for item in stale_overlays:
             rospy.logdebug(f"Removing overlay from active \n\t{item}")
-            if self.active_overlays.pop(item, None) is None:
+            stale_overlay = self.active_overlays.pop(item, None)
+            if stale_overlay is None:
                 rospy.logerr(f"Error removing expected active overlay: {item}")
+            stale_overlay.set_state(ApplicationState.STOPPED)
 
         for item in new_overlays:
             rospy.logdebug(f"Adding overlay to active \n\t{item}")
-            self.active_overlays[item] = True
-
-        self.director_pub.publish(make_director_message(self.active_overlays.keys()))
+            self.active_overlays[item] = self._make_player(item)
 
         rospy.loginfo(
             f"Finshed message handling. | "
             f"Active overlays: {len(self.active_overlays)}"
         )
 
-    def _make_url(self, stream_id):
-        return(
-            f"http://localhost:8008/lg_media/webapps/janusplayer/index.html?"
-            f"janusHost={self.janus_host}&janusPort={self.janus_port}&streamID={stream_id}"
-        )
+    @staticmethod
+    def _make_player(stream_info):
+        """Returns a new ManagedApplication for the stream message"""
+        player = Player(stream_info=stream_info)
+        player.set_state(ApplicationState.STARTED)
+        player.set_state(ApplicationState.VISIBLE)
 
+        return player
 
-def make_director_message(overlays):
-    browser_windows = []
-    msg = GenericMessage()
-    msg.type = "json"
-
-    for overlay in overlays:
-        geometry = ManagedWindow.lookup_viewport_geometry(overlay.viewport)
-        browser_windows.append(
-            {
-                "activity": "browser",
-                "activity_config": {},
-                "assets": [overlay.url],
-                "height": geometry.height,
-                "presentation_viewport": overlay.viewport,
-                "width": geometry.width,
-                "x_coord": 0,
-                "y_coord": 0
-            }
-        )
-
-    director_msg = {
-        "description": "testdescription",
-        "duration": 0,
-        "name": "Media Overlays",
-        "resource_uri": "/this/isnt/real",
-        "slug": "current_overlays",
-        "windows": browser_windows
-    }
-
-    msg.message = json.dumps(director_msg)
-
-    return msg
 
 def main():
-    rospy.init_node('media_launcher', log_level=rospy.INFO)
-
-    janus_host = rospy.get_param('~janus_host', 'localhost')
-    janus_port = rospy.get_param('~janus_port', 8088)
+    rospy.init_node('media_launcher', log_level=rospy.DEBUG)
 
     viewports = [param.strip() for param in rospy.get_param('~viewports', '').split(',')]
-    overlay_director_pub = rospy.Publisher('director/scene', GenericMessage, queue_size=10)
-    launcher = MediaLauncher(janus_host, janus_port, overlay_director_pub, viewports)
-
+    launcher = MediaLauncher(viewports)
     rospy.Subscriber('/media_overlays', MediaOverlays, launcher.handle_message)
 
     rospy.loginfo(f"Started media_launcher node on topic /media_overlays for viewports {viewports}")
