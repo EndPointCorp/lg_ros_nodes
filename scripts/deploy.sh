1d33e034591fe6cbc0bb9b7d574dad6ad3f2d176
Will Plaut <will@endpoint.com>
2020-05-05 10:20:56 -0400

    Fix deploy path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ chmod 0600 /root/.ssh/id_rsa
 eval $(ssh-agent -s)
 
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} mkdir -p incoming/lg_ros_nodes/origin/master/
-cd /src/lg_ros_nodes/catkin/debs
+cd /src/lg_ros_nodes/catkin/
 scp -o StrictHostKeychecking=no ./debs/*.deb aptly@${APTLY_SERVER}:incoming/lg_ros_nodes/origin/master/
 ssh -o StrictHostKeychecking=no  aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming.sh --project lg_ros_nodes --branch origin/master --rosrel "melodic" --distro "bionic"
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming-separate-repos.sh --project lg_ros_nodes --branch origin/master --rosrel "melodic" --distro "bionic"
