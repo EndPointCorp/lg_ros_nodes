9fb997e2aaa32d233bbcbee6df724cdd254c6543
Will Plaut <will@endpoint.com>
2020-05-28 16:42:07 -0400

    deploy.sh: fix aptly naming because

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,6 @@ eval $(ssh-agent -s)
 
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} mkdir -p incoming/lg_ros_nodes/origin/master/
 cd /src/lg_ros_nodes/catkin/
-scp -o StrictHostKeychecking=no ./debs/*.deb aptly@${APTLY_SERVER}:incoming/lg_ros_nodes/origin/master/
+scp -o StrictHostKeychecking=no ./debs/*.deb aptly@${APTLY_SERVER}:incoming/lg-ros/origin/master/
 ssh -o StrictHostKeychecking=no  aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming.sh --project lg-ros --branch origin/master --rosrel "melodic" --distro "bionic"
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming-separate-repos.sh --project lg-ros --branch origin/master --rosrel "melodic" --distro "bionic"
