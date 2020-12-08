b5aeb6bcf0ba0088c273dc1943f4875e5298cd32
dkiselev <dmitry.v.kiselev@gmail.com>
2020-12-08 17:06:10 -0400

    relaunch browser on aw snap

--- lg_common/src/lg_common/managed_browser.py
+++ lg_common/src/lg_common/managed_browser.py
@@ -251,6 +251,7 @@ class ManagedBrowser(ManagedApplication):
     def check_alive_and_reload(self):
         if not self.check_alive():
             rospy.logerr("Browser is probably dead")
+            self.proc.handle_soft_relaunch()
 
     def check_alive(self):
         return len(self.list_pages_available_for_debug()) > 0
