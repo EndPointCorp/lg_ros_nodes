7e7340b3b2bdc481c347718e4b08b56225548e4f
Alejandro Ramon <aramon@endpointdev.com>
2022-03-24 14:51:21 -0600

    Further improvements to navtransform ros node, confirmed with testing at CBRE

--- lg_navlib/scripts/navtransform.py
+++ lg_navlib/scripts/navtransform.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist, PoseStamped
 
 
 class NavTransform(object):
-    """ emit /lg_navlib/twist messages with 'pan x' where 'zoom' detected """
+    """ correct /lg_navlib/twist messages into natural movement, emit on /navtransform/twist """
     def __init__(self):
         self.node = rospy.init_node('navtransform')
         self.puber = rospy.Publisher('/navtransform/twist', Twist, queue_size=10)
@@ -14,15 +14,17 @@ class NavTransform(object):
         self.sub_pose = rospy.Subscriber("/earth/pose", PoseStamped, self.update_pose)
         self.tilt = 0.0
         self.altitude = 0.0
-        self.lastrotate = 0.0
 
     def update_pose(self, pose):
-        """ update tilt, any other values from pose """
+        """ update tilt and altitude from /earth/pose """
         self.tilt = pose.pose.orientation.x
         self.altitude = pose.pose.position.z
 
     def transform_twist(self, twist):
         """ fix twist, publish """
+#       # increase zoom speed with tilt
+        twist.linear.z = (twist.linear.z + (twist.linear.z * max(self.tilt, 0.01) / 90))
+
 #       # convert zoom into panned-zoom
         down = min(max(twist.linear.z, -1), 1) * -1
         tiltness = min(1.0, max(0.0, self.tilt / 90))
@@ -30,39 +32,29 @@ class NavTransform(object):
             twist.linear.x = twist.linear.x * (1 - tiltness) + down * tiltness
             twist.linear.z *= 1.0 - tiltness
 
-#       # correct pan
-        twist.linear.x /= 2
-
-#       # limit tilt-up to 65-60
+#       # limit tilt-up to 85-90
         if twist.angular.y < 0:
-            if self.tilt > 70:
+            if self.tilt > 90:
                 twist.angular.y = 0.05
-            elif self.tilt > 65:
+            elif self.tilt > 85:
                 twist.angular.y = 0.0
-            elif self.tilt >= 55:
-                twist.angular.y /= 3
+#           # slow down tilt-up before topping off
+            elif self.tilt >= 75:
+                twist.angular.y *= 0.75
 
 #       # add pan when tilting
-        if twist.angular.y != 0 and self.tilt > 0:
-            twist.angular.y /= 4
-            twist.linear.x = 1.25 * twist.angular.y * self.tilt / 90
+        if twist.angular.y != 0 and self.tilt > 0 and self.altitude < 500000:
+            twist.linear.x = twist.angular.y * self.tilt / 90
 
 #       # correct tilt when zooming-in close to the ground
-        if twist.linear.z < 0 and self.altitude < 10000 and self.tilt < 50:
-            twist.angular.y = twist.linear.z / 2
+        if twist.linear.z < 0 and self.altitude < 3000 and self.tilt < 70:
+            twist.angular.y = 1.5 * twist.linear.z
 
+#       # correct position while rotating to keep subject on screen
         if abs(twist.angular.z) > 0:
-#            # ignore random big values that come in :P
-            if not abs(self.lastrotate) * 2 > abs(twist.angular.z) > abs(self.lastrotate) / 2:
-                self.lastrotate = twist.angular.z
-                twist.angular.z = 0.0
-            else:
-                self.lastrotate = twist.angular.z
-                twist.angular.z /= 2
-                twist.linear.z, twist.angular.x, twist.angular.y = 0.0, 0.0, 0.0
-#               # correct position while rotating to keep subject on screen
-                twist.linear.y = -1 * twist.angular.z * self.tilt / 90
-                twist.linear.x = twist.linear.y / 4
+            twist.angular.z *= 0.75
+            twist.linear.y = -1 * twist.angular.z * self.tilt / 90
+            twist.linear.x = twist.linear.y / 4
 
 #       # correct tilt when zooming-out to keep earth on screen
         if twist.linear.z > 0 and self.altitude > 1000000 and self.tilt > 0:
