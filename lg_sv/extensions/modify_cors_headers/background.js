7ee996738cac5bb54b8895c6c12bf082ca4af14b
Kannan Ponnusamy <kannan@endpointdev.com>
2022-05-27 07:58:56 -0400

    Fixed the streetview issues caused by duplicate CORS headers

--- lg_sv/extensions/modify_cors_headers/background.js
+++ lg_sv/extensions/modify_cors_headers/background.js
@@ -1,8 +1,17 @@
+function removeHeader(headers, name) {
+  for (var i = 0; i < headers.length; i++) {
+    if (headers[i].name.toLowerCase() == name) {
+      headers.splice(i, 1);
+      break;
+    }
+  }
+}
+
 chrome.webRequest.onHeadersReceived.addListener(
   function(details) {
     const allowMethods = {name:"Access-Control-Allow-Methods", value:"GET, PUT, POST, DELETE, HEAD, OPTIONS, PATCH"};
     const allowOrigin = {name:"Access-Control-Allow-Origin", value:"*"};
-
+    removeHeader(details.responseHeaders,'access-control-allow-origin');
     const responseHeaders = details.responseHeaders.concat(allowOrigin, allowMethods);
     return { responseHeaders };
   },
