6ce0e631638cf6bde907063c9d099c16dee48048
Constante "Tino" Gonzalez <tino@endpointdev.com>
2024-03-13 14:39:02 -0400

    almost no transform in navtransform

--- lg_navlib/scripts/navtransform.py
+++ lg_navlib/scripts/navtransform.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import math
 import rospy
 from geometry_msgs.msg import Twist, PoseStamped
 
@@ -18,47 +17,12 @@ class NavTransform(object):
     def update_pose(self, pose):
         """ update tilt and altitude from /earth/pose """
         self.tilt = pose.pose.orientation.x
-        self.altitude = pose.pose.position.z
+#        self.altitude = pose.pose.position.z
 
     def transform_twist(self, twist):
         """ fix twist, publish """
-#       # increase zoom speed with tilt
+#       increase zoom speed with tilt
         twist.linear.z = (twist.linear.z + (twist.linear.z * max(self.tilt, 0.01) / 90))
-
-#       # convert zoom into panned-zoom
-        down = min(max(twist.linear.z, -1), 1) * -1
-        tiltness = min(1.0, max(0.0, self.tilt / 90))
-        if twist.linear.z != 0:
-            twist.linear.x = twist.linear.x * (1 - tiltness) + down * tiltness
-            twist.linear.z *= 1.0 - tiltness
-
-#       # limit tilt-up to 85-90
-        if twist.angular.y < 0:
-            if self.tilt > 90:
-                twist.angular.y = 0.05
-            elif self.tilt > 85:
-                twist.angular.y = 0.0
-#           # slow down tilt-up before topping off
-            elif self.tilt >= 75:
-                twist.angular.y *= 0.75
-
-#       # add pan when tilting
-        if twist.angular.y != 0 and self.tilt > 0 and self.altitude < 500000:
-            twist.linear.x = twist.angular.y * self.tilt / 90
-
-#       # correct tilt when zooming-in close to the ground
-        if twist.linear.z < 0 and self.altitude < 3000 and self.tilt < 70:
-            twist.angular.y = 1.5 * twist.linear.z
-
-#       # correct position while rotating to keep subject on screen
-        if abs(twist.angular.z) > 0:
-            twist.angular.z *= 0.75
-            twist.linear.y = -1 * twist.angular.z * self.tilt / 90
-            twist.linear.x = twist.linear.y / 4
-
-#       # correct tilt when zooming-out to keep earth on screen
-        if twist.linear.z > 0 and self.altitude > 1000000 and self.tilt > 0:
-            twist.angular.y = 0.4
 
         self.puber.publish(twist)
 
