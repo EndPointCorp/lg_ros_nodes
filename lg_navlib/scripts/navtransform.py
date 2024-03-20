a5de4d9d64b6dc309f9a803bcf7da603d4d08fb3
Constante "Tino" Gonzalez <tino@endpointdev.com>
2024-03-20 13:45:14 -0400

    navtransform read and use param for 'touch multiplier'

--- lg_navlib/scripts/navtransform.py
+++ lg_navlib/scripts/navtransform.py
@@ -13,7 +13,7 @@ class NavTransform(object):
         self.sub_pose = rospy.Subscriber("/earth/pose", PoseStamped, self.update_pose)
         self.tilt = 0.0
         self.altitude = 0.0
-
+        self.speed_multiplier = speed_multiplier
     def update_pose(self, pose):
         """ update tilt and altitude from /earth/pose """
         self.tilt = pose.pose.orientation.x
@@ -22,12 +22,14 @@ class NavTransform(object):
     def transform_twist(self, twist):
         """ fix twist, publish """
 #       increase zoom speed with tilt
-        twist.linear.z = (twist.linear.z + (2 * (twist.linear.z * max(self.tilt, 0.01) / 90)))
-        twist.linear.x = twist.linear.x * 2
-        twist.linear.y = twist.linear.y * 2
+        twist.linear.z = (twist.linear.z + (self.speed_multiplier * (twist.linear.z * max(self.tilt, 0.01) / 90)))
+#       increase pan speed
+        twist.linear.x = twist.linear.x * self.speed_multiplier
+        twist.linear.y = twist.linear.y * self.speed_multiplier
         self.puber.publish(twist)
 
 
 if __name__ == '__main__':
-    NavTransform()
+    speed_multiplier = rospy.get_param("/navtransform/speed_multiplier", 2)
+    NavTransform(speed_multiplier)
     rospy.spin()
