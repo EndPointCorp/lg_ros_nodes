b5b02e8272595bcf67b973ab9b1e607a10fcb1ab
Constante "Tino" Gonzalez <tino@endpointdev.com>
2024-03-26 14:06:41 -0400

    navtransform correct param source

--- lg_navlib/scripts/navtransform.py
+++ lg_navlib/scripts/navtransform.py
@@ -6,23 +6,22 @@ from geometry_msgs.msg import Twist, PoseStamped
 
 class NavTransform(object):
     """ correct /lg_navlib/twist messages into natural movement, emit on /navtransform/twist """
-    def __init__(self, speed_multiplier):
+    def __init__(self):
+        self.speed_multiplier = rospy.get_param("/navtransform_42_a/speed_multiplier", 1)
         self.node = rospy.init_node('navtransform')
         self.puber = rospy.Publisher('/navtransform/twist', Twist, queue_size=10)
         self.sub_twist = rospy.Subscriber("/lg_navlib/twist", Twist, self.transform_twist)
         self.sub_pose = rospy.Subscriber("/earth/pose", PoseStamped, self.update_pose)
         self.tilt = 0.0
         self.altitude = 0.0
-        self.speed_multiplier = speed_multiplier
+
     def update_pose(self, pose):
         """ update tilt and altitude from /earth/pose """
         self.tilt = pose.pose.orientation.x
-#        self.altitude = pose.pose.position.z
+#       self.altitude = pose.pose.position.z
 
     def transform_twist(self, twist):
         """ fix twist, publish """
-#       increase zoom speed with tilt
-        twist.linear.z = (twist.linear.z + (self.speed_multiplier * (twist.linear.z * max(self.tilt, 0.01) / 90)))
 #       increase pan speed
         twist.linear.x = twist.linear.x * self.speed_multiplier
         twist.linear.y = twist.linear.y * self.speed_multiplier
@@ -30,6 +29,5 @@ class NavTransform(object):
 
 
 if __name__ == '__main__':
-    speed_multiplier = rospy.get_param("/navtransform/speed_multiplier", 2)
-    NavTransform(speed_multiplier)
+    NavTransform()
     rospy.spin()
